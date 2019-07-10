70f6f17c58906fa452479aa8912c8036958c49b1
HolySoft_Ayan <betaplayer12@gmail.com>
2019-07-10 11:02:58 +0800

    [+]Moving PoolableItemData from IPoolableItem script

---
+++ Assets/Holysoft/Scripts/Pooling/PoolableItemData.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Holysoft.Pooling
+{
+    [CreateAssetMenu(fileName = "PoolableItemData",menuName = "Holysoft/Basic Pool Data")]
+    public class PoolableItemData : ScriptableObject
+    {
+
+    }
+
+   
+}
