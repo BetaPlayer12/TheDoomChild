de17615c0d1169cdf5dee8c99ddff61c56e40b39
Jhaypee Pontz <johnpaulpontino@gmail.com>
2019-07-25 17:14:42 +0800

    Pulling from my local repo

---
+++ Assets/DChild/Scripts/Gameplay/Characters/Player/State/IPlayerAnimationState.cs
@@ -0,0 +1,29 @@
+﻿
+
+namespace DChild.Gameplay.Characters.Players.State
+{
+    public interface IPlayerAnimationState
+    {
+        bool isFromFall { get; set; }
+        bool isAnticPlayed { get; set; }
+        bool hasDoubleJumped { get; set; }
+        bool hasAttacked { get; set; }
+        bool isLanding { get; set; }
+        bool isFallingToJog { get; set; }
+        bool isFromJog { get; set; }
+        bool hasJumped { get; set; }
+        bool isShortJumping { get; set; }
+        bool isFromIdle { get; set; }
+        bool isStaticJumping { get; set; }
+        bool hasDashed { get; set; }
+        bool hasWallSticked { get; set; }
+        bool isWallJumping { get; set; }
+        bool onWallJumpEnd { get; set; }
+        bool isThrowingBomb { get; set; }
+        bool transitionToFall2 { get; set; }
+        bool isHardLanding { get; set; }
+        bool isFallingFromWallJump { get; set; }
+        bool isLedging { get ; set; }
+        void ResetAnimations();
+    }
+}
---
+++ Assets/Debug/Skills/Combat/VisualEffects.cs
@@ -0,0 +1,103 @@
+﻿using DChild.Gameplay;
+using DChild.Gameplay.Characters;
+using DChild.Gameplay.Characters.Players;
+using DChild.Gameplay.Characters.Players.Behaviour;
+using DChild.Gameplay.Characters.Players.Modules;
+using DChild.Gameplay.Characters.Players.State;
+using DChild.Inputs;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+public class VisualEffects : MonoBehaviour, IPlayerExternalModule
+{
+    #region FX
+    [SerializeField]
+    [TabGroup("Fx")]
+    private GameObject m_fxPrefab;
+    [SerializeField]
+    [TabGroup("Fx")]
+    private int m_quantity;
+    [SerializeField]
+    [TabGroup("Fx")]
+    private bool m_generateSlashFx;
+    [SerializeField]
+    [TabGroup("Fx")]
+    private bool m_generateWhipFx;
+    #endregion
+
+    #region SpawnPoints
+    [SerializeField]
+    [TabGroup("SpawnPoints")]
+    private Transform m_up;
+    [SerializeField]
+    [TabGroup("SpawnPoints")]
+    private Transform m_forward;
+    [SerializeField]
+    [TabGroup("SpawnPoints")]
+    private Transform m_down;
+    #endregion
+
+    private CombatController m_combatController;
+    private CharacterPhysics2D m_physics;
+    private PlayerInput m_input;
+
+    private ICombatState m_combatState;
+    private IPlayerState m_playerState;
+
+    private int m_count;
+
+    public void Initialize(IPlayerModules player)
+    {
+        m_playerState = player.characterState;
+        m_combatState = player.characterState;
+        m_physics = player.physics;
+    }
+
+    private void Update()
+    {
+        if (m_combatState.isAttacking)
+        {
+            if (m_combatController.isMainHandPressed && m_generateSlashFx)
+            {
+                GenerateFx();
+            }
+            else if (m_combatController.isOffHandPressed && m_generateWhipFx)
+            {
+                GenerateFx();
+            }
+        }
+        else
+        {
+            m_count = m_quantity;
+        }
+    }
+
+    private void GenerateFx()
+    {
+        var spawnPos = transform.position;
+        if (m_playerState.isCrouched)
+        {
+            spawnPos = m_down.position;
+        }
+        else if (m_input.direction.isUpHeld)
+        {
+            spawnPos = m_up.position;
+        }
+        else
+        {
+            spawnPos = m_forward.position;
+        }
+
+        if (m_count != 0)
+        {
+            Instantiate(m_fxPrefab, spawnPos, Quaternion.identity);
+            m_count--;
+        }
+    }
+
+    private void Start()
+    {
+        m_combatController = m_physics.GetComponentInChildren<CombatController>();
+        m_input = GetComponentInParent<PlayerInput>();
+    }
+}
