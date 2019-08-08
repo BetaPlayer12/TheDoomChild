9b7e975c8ae73777103af2917e6d5ebe5b2f85b1
HolySoft_Ayan <betaplayer12@gmail.com>
2019-08-08 17:20:42 +0800

    [+] Setting up Soul Crystal

---
+++ Assets/DChild/Scripts/Gameplay/System/PlayerSystems/SoulSkill/Challenge.cs
@@ -0,0 +1,11 @@
+﻿#if UNITY_EDITOR
+#endif
+
+namespace DChild.Gameplay.Characters.Players.SoulSkills
+{
+    public interface Challenge
+    {
+        string message { get; }
+        bool IsComplete();
+    }
+}
---
+++ Assets/DChild/Scripts/Gameplay/System/PlayerSystems/SoulSkill/SoulCrystal.cs
@@ -0,0 +1,79 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+using DChildEditor;
+#endif
+
+namespace DChild.Gameplay.Characters.Players.SoulSkills
+{
+    [CreateAssetMenu(fileName = "SoulCrystal", menuName = "DChild/Database/Soul Crystal")]
+    public class SoulCrystal : SerializedScriptableObject
+    {
+        [SerializeField, OnValueChanged("UpdateReference")]
+        private SoulSkill m_activateSoulSkill;
+        [SerializeField, ReadOnly]
+        private string m_name;
+        [SerializeField]
+        private Sprite m_icon;
+        [SerializeField]
+        private Challenge[] m_challenges;
+
+
+        public Sprite icon => m_icon;
+        public string crystalName => m_name;
+        public string challengeInfo
+        {
+            get
+            {
+                string message = "";
+                for (int i = 0; i < m_challenges.Length; i++)
+                {
+                    message += m_challenges[i].message;
+                }
+                return message;
+            }
+        }
+
+        public bool IsQualifiedToActiveSkill()
+        {
+            for (int i = 0; i < m_challenges.Length; i++)
+            {
+                if (m_challenges[i].IsComplete() == false)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public void ActivateSoulSkill()
+        {
+
+        }
+
+#if UNITY_EDITOR
+        private void UpdateReference()
+        {
+            if (m_activateSoulSkill == null)
+            {
+                m_name = "Unknown Crystal";
+            }
+            else
+            {
+                m_name = m_activateSoulSkill.name + " Crystal";
+            }
+            UpdateAssetName();
+
+        }
+
+        private void UpdateAssetName()
+        {
+            string assetPath = AssetDatabase.GetAssetPath(GetInstanceID());
+            var fileName = m_name.Replace(" ", string.Empty);
+            FileUtility.RenameAsset(this, assetPath, fileName);
+            AssetDatabase.SaveAssets();
+        }
+#endif
+    }
+}
