27f2e9ead8bf535b7db5b6621bca2b90be09ed73
HolySoft_Ayan <betaplayer12@gmail.com>
2019-09-16 13:49:27 +0800

    misc files

---
+++ Assets/DChild/Editor/DevelopmentOnly/ToolKits/UnityExportsToolKit.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnityExportsToolKit
+{
+    public void Export(RenderTexture renderTexture, TextureFormat textureFormat, string filePath, string fileName)
+    {
+        Texture2D virtualPhoto =
+            new Texture2D(renderTexture.width, renderTexture.height, textureFormat, false);
+        // false, meaning no need for mipmaps
+        virtualPhoto.ReadPixels(new Rect(0, 0, renderTexture.width, renderTexture.height), 0, 0);
+
+        byte[] bytes;
+        bytes = virtualPhoto.EncodeToPNG();
+
+        System.IO.File.WriteAllBytes(
+            $"{filePath}/{filePath}.png", bytes);
+    }
+}
