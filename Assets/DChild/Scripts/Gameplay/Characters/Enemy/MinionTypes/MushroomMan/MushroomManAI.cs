a1724d8f09047761c7fd0cabacc806185141084e
Jhaypee Pontz <johnpaulpontino@gmail.com>
2019-09-20 07:53:16 +0800

    Updating Minions Audio

--- Assets/DChild/Scripts/Gameplay/Characters/Enemy/MinionTypes/MushroomMan/MushroomManAI.cs
+++ Assets/DChild/Scripts/Gameplay/Characters/Enemy/MinionTypes/MushroomMan/MushroomManAI.cs
@@ -48,6 +48,7 @@ namespace DChild.Gameplay.Characters.Enemies
             private string m_deathAnimation;
             public string deathAnimation => m_deathAnimation;
 
+
             public override void Initialize()
             {
 #if UNITY_EDITOR
@@ -92,6 +93,14 @@ namespace DChild.Gameplay.Characters.Enemies
 
         [ShowInInspector]
         private StateHandle<State> m_stateHandle;
+
+
+        [SerializeField]
+        private AudioSource m_Audiosource;
+        [SerializeField]
+        private AudioClip m_AttackClip;
+        [SerializeField]
+        private AudioClip m_DeadClip;
 
         private void OnAttackDone(object sender, EventActionArgs eventArgs)
         {
@@ -138,6 +147,8 @@ namespace DChild.Gameplay.Characters.Enemies
 
         protected override void OnDestroyed(object sender, EventActionArgs eventArgs)
         {
+            m_Audiosource.clip = m_DeadClip;
+            m_Audiosource.Play();
             base.OnDestroyed(sender, eventArgs);
             m_movement.Stop();
         }
@@ -194,6 +205,10 @@ namespace DChild.Gameplay.Characters.Enemies
                     m_animation.EnableRootMotion(true, true);
                     m_attackHandle.ExecuteAttack(m_info.attack.animation);
                     m_animation.AddAnimation(0, m_info.idleAnimation, true, 0);
+                    //Audio Play need changes
+                    m_Audiosource.clip = m_AttackClip;
+                    m_Audiosource.Play();
+                    
                     break;
                 case State.Chasing:
                     {
