c82c40a665ae5e387bf4421471a9277be4969b15
animyannie <francine.villanueva4@gmail.com>
2019-09-30 17:25:54 +0800

    1. Created Bird Assets for the environment with 3 different Scenes 2. Deleted the high resolution Smoke image. wasnt using it anymore

---
+++ Assets/DChild/Scripts/Gameplay/EnvironmentTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace DChild.Gameplay
+{
+    public class EnvironmentTrigger : MonoBehaviour
+    {
+        [SerializeField]
+        private UnityEvent m_events;
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if(collision.CompareTag("Sensor") == false)
+            {
+                m_events?.Invoke();
+            }
+        }
+    }
+}
