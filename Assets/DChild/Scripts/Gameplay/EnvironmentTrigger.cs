242f59dabe2b748da9876abdc88b3f95c06df5b2
animyannie <francine.villanueva4@gmail.com>
2019-10-04 17:22:34 +0800

    1. Fixed the crow animation stages 2. Revised the Sky color of the Boss Stage Phase 4 3. Revised the color of the assets on Phase 1 - 5 4. deleted thron wall asset, waiting for final asset.

--- Assets/DChild/Scripts/Gameplay/EnvironmentTrigger.cs
+++ Assets/DChild/Scripts/Gameplay/EnvironmentTrigger.cs
@@ -8,13 +8,19 @@ namespace DChild.Gameplay
     public class EnvironmentTrigger : MonoBehaviour
     {
         [SerializeField]
+        private bool m_oneTimeOnly;
+        [SerializeField]
         private UnityEvent m_events;
 
         private void OnTriggerEnter2D(Collider2D collision)
         {
-            if(collision.CompareTag("Sensor") == false)
+            if(collision.CompareTag("Hitbox"))
             {
                 m_events?.Invoke();
+                if (m_oneTimeOnly)
+                {
+                    enabled = false;
+                }
             }
         }
     }
