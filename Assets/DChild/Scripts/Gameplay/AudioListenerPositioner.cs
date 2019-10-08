397a7cce460a02bef0ca640c4a8b9817bfb1f1bd
HolySoft_Ayan <betaplayer12@gmail.com>
2019-10-08 14:26:47 +0800

    [-] Deleting Old Scripts [+] Adding Audio Listener handling for master audio

---
+++ Assets/DChild/Scripts/Gameplay/AudioListenerPositioner.cs
@@ -0,0 +1,28 @@
+﻿using DChild;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioListenerPositioner : MonoBehaviour
+{
+    private Transform m_camera;
+
+    private void OnCameraChange(object sender, CameraChangeEventArgs eventArgs)
+    {
+        m_camera = eventArgs.camera?.transform ?? null;
+    }
+
+    private void Start()
+    {
+        m_camera = GameSystem.mainCamera?.transform ?? null;
+        GameSystem.CameraChange += OnCameraChange;
+    }
+
+    private void LateUpdate()
+    {
+        var position = m_camera.position;
+        position.z = 0;
+        transform.position = position;
+    }
+}
