2c910c0778aa15046dda397f25208abdef088dd3
HolySoft_Ayan <betaplayer12@gmail.com>
2019-10-10 13:38:25 +0800

    [-] Deleting Old Minon Prefab [*] Improving MouseCursor

--- Assets/DChild/Scripts/Game/Cursor.cs
+++ Assets/DChild/Scripts/Game/Cursor.cs
@@ -20,10 +20,17 @@ namespace DChild
         private Vector2 m_offset;
         [SerializeField]
         private float m_forwardOffset;
+        [SerializeField]
+        private int m_referenceFeildOfView;
 
         private Vector3 m_offset3D;
 
         private Camera m_cacheCamera;
+        private Transform m_cacheTransform;
+        private Vector3 m_previousMousePosition;
+        private Vector3 m_previousCameraPosition;
+        private Quaternion m_previousCameraRotation;
+        private float m_previousCameraFieldOfView;
 
         public void SetVisibility(bool isVisible)
         {
@@ -36,47 +43,68 @@ namespace DChild
             m_offset3D = new Vector3(m_offset.x, m_offset.y, 0);
         }
 
-        private void Awake()
+        private void ChangeCamera(Camera camera)
         {
-            m_offset3D = new Vector3(m_offset.x, m_offset.y, 0);
-            m_cacheCamera = GameSystem.mainCamera;
-            GameSystem.CameraChange += OnCameraChange;
+            m_cacheCamera = camera;
+            m_cacheTransform = m_cacheCamera?.transform ?? null;
+            FollowCursor(Input.mousePosition, m_cacheTransform.rotation, m_cacheTransform.position, m_cacheCamera.fieldOfView);
             enabled = m_cacheCamera;
-#if UNITY_EDITOR
-            SetVisibility(m_makeSystemCursorVisible);
-#endif
         }
 
         private void OnCameraChange(object sender, CameraChangeEventArgs eventArgs)
         {
-            m_cacheCamera = eventArgs.camera;
-            enabled = m_cacheCamera;
+            ChangeCamera(eventArgs.camera);
         }
 
-        public void LateUpdate()
+        private void FollowCursor(Vector3 currentMousePosition, Quaternion currentRotation, Vector3 currentPosition, float currentFieldOfView)
         {
-            if (m_cacheCamera)
+            if (m_previousMousePosition != currentMousePosition || m_previousCameraRotation != currentRotation ||
+                m_previousCameraPosition != currentPosition || m_previousCameraFieldOfView != currentFieldOfView)
             {
-                var position = Input.mousePosition;
-                transform.rotation = m_cacheCamera.transform.rotation;
+                transform.rotation = m_cacheTransform.rotation;
                 if (m_cacheCamera.orthographic)
                 {
-                    var worldPosition = m_cacheCamera.ScreenToWorldPoint(position); //We are on the camera's position itself
-                    worldPosition += m_cacheCamera.transform.forward * m_forwardOffset;
+                    var worldPosition = m_cacheCamera.ScreenToWorldPoint(currentMousePosition); //We are on the camera's position itself
+                    worldPosition += m_cacheTransform.forward * m_forwardOffset;
                     worldPosition += m_offset3D;
                     transform.position = worldPosition;
                 }
                 else
                 {
-                    Ray ray = m_cacheCamera.ScreenPointToRay(position);
-                    var cameraForward = m_cacheCamera.transform.forward;
+                    Ray ray = m_cacheCamera.ScreenPointToRay(currentMousePosition);
+                    var cameraForward = m_cacheTransform.forward;
                     var point = m_cacheCamera.transform.position + (cameraForward * m_forwardOffset);  //Positioning Plane Infront of Camera;
                     Plane xy = new Plane(cameraForward, point);
                     float distance;
                     xy.Raycast(ray, out distance);
                     Debug.DrawRay(ray.origin, ray.direction * distance);
-                    transform.position = ray.GetPoint(distance) + m_offset3D;
+                    var xOffset = m_cacheTransform.right * m_offset3D.x;
+                    var yOffset = m_cacheTransform.up * m_offset3D.y;
+                    var worldOffset = ((xOffset + yOffset) * m_cacheCamera.fieldOfView / m_referenceFeildOfView);
+                    transform.position = ray.GetPoint(distance) + worldOffset;
                 }
+                m_previousMousePosition = currentMousePosition;
+                m_previousCameraRotation = currentRotation;
+                m_previousCameraPosition = currentPosition;
+                m_previousCameraFieldOfView = currentFieldOfView;
+            }
+        }
+
+        private void Awake()
+        {
+            m_offset3D = new Vector3(m_offset.x, m_offset.y, 0);
+            ChangeCamera(GameSystem.mainCamera);
+            GameSystem.CameraChange += OnCameraChange;
+#if UNITY_EDITOR
+            SetVisibility(m_makeSystemCursorVisible);
+#endif
+        }
+
+        private void LateUpdate()
+        {
+            if (m_cacheCamera)
+            {
+                FollowCursor(Input.mousePosition, m_cacheTransform.rotation, m_cacheTransform.position, m_cacheCamera.fieldOfView);
             }
             else
             {
