b7d6b454006f44166c0e5fe68e48d4bb0013dfaa
TWICE Jihyan <giancarlo.toledo555@gmail.com>
2019-11-21 14:18:29 +0800

    [*] more minion pushes

--- Assets/DChild/Scripts/Gameplay/Characters/Enemy/MinionTypes/BeeDrones/RangeBeeDroneAI.cs
+++ Assets/DChild/Scripts/Gameplay/Characters/Enemy/MinionTypes/BeeDrones/RangeBeeDroneAI.cs
@@ -264,22 +264,29 @@ namespace DChild.Gameplay.Characters.Enemies
 
         private void LaunchStingerProjectile()
         {
-            var target = playerPosition; //No Parabola      
-            Vector2 spitPos = m_stingerPos.position;
-            Vector3 v_diff = (target - spitPos);
-            float atan2 = Mathf.Atan2(v_diff.y, v_diff.x);
-            if (m_info.delayShotTimer <= timeCounter)
+            if (IsFacingTarget())
             {
-                postAtan2 = atan2;
-                timeCounter = 0;
-                Debug.Log("update Check");
+                var target = m_targetInfo.position; //No Parabola      
+                Vector2 spitPos = m_stingerPos.position;
+                Vector3 v_diff = (target - spitPos);
+                float atan2 = Mathf.Atan2(v_diff.y, v_diff.x);
+                if (m_info.delayShotTimer <= timeCounter)
+                {
+                    postAtan2 = atan2;
+                    timeCounter = 0;
+                    Debug.Log("update Check");
+                }
+
+                m_stingerPos.rotation = Quaternion.Euler(0f, 0f, postAtan2 * Mathf.Rad2Deg);
+                GameObject burst = Instantiate(m_info.burstGO, spitPos, m_stingerPos.rotation);
+                m_stingerLauncher.LaunchProjectile();
+                //m_Audiosource.clip = m_RangeAttackClip;
+                //m_Audiosource.Play();
             }
-           
-            m_stingerPos.rotation = Quaternion.Euler(0f, 0f, postAtan2 * Mathf.Rad2Deg);
-            GameObject burst = Instantiate(m_info.burstGO, spitPos, m_stingerPos.rotation);
-            m_stingerLauncher.LaunchProjectile();
-            //m_Audiosource.clip = m_RangeAttackClip;
-            //m_Audiosource.Play();
+            else
+            {
+                m_stateHandle.OverrideState(State.Turning);
+            }
         }
         
         protected override void Awake()
@@ -350,7 +357,7 @@ namespace DChild.Gameplay.Characters.Enemies
                             {
 
                                 var target = m_targetInfo.position;
-                                target.y -= 0.5f;
+                                //target.y -= 0.5f;
                                 m_animation.DisableRootMotion();
                                 if (m_character.physics.velocity != Vector2.zero)
                                 {
