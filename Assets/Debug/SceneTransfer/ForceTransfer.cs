5c8038698800381367fdb27b2f6a6a7893216559
TWICE Jihyan <giancarlo.toledo555@gmail.com>
2020-01-21 17:49:31 +0800

    ....

--- Assets/Debug/SceneTransfer/ForceTransfer.cs
+++ Assets/Debug/SceneTransfer/ForceTransfer.cs
@@ -9,16 +9,16 @@ namespace DChildDebug.Gameplay
 {
     public class SceneTransferDebugger : MonoBehaviour
     {
-        [SerializeField]
-        private LocationSwitcher m_switcher;
-        [SerializeField]
-        private Character m_character;
+        //[SerializeField]
+        //private LocationSwitcher m_switcher;
+        //[SerializeField]
+        //private Character m_character;
 
-        [Button]
-        public void Switch()
-        {
-            m_switcher.GoToDestination(m_character);
-        }
+        //[Button]
+        //public void Switch()
+        //{
+        //    m_switcher.GoToDestination(m_character);
+        //}
     }
 
 }
