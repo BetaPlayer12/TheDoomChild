0296ecc4eeb86c07dade67b4492279c7dc37d2c9
HolySoft_Ayan <betaplayer12@gmail.com>
2020-02-10 18:17:29 +0800

    [+] Breakable Jars, Floor and Wall created/updated, with its working prefab

---
+++ Assets/DChild/Scripts/SpriteUtility.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace DChild
+{
+    public class SpriteUtility : MonoBehaviour
+    {
+        private SpriteRenderer m_renderer;
+        private Sprite m_savedSprite;
+
+        public void SaveCurrentSprite()
+        {
+            m_savedSprite =  m_renderer.sprite;
+        }
+
+        public void LoadSavedSprite()
+        {
+            m_renderer.sprite = m_savedSprite;
+        }
+
+        private void Awake()
+        {
+            m_renderer = GetComponent<SpriteRenderer>();
+        }
+    }
+}
