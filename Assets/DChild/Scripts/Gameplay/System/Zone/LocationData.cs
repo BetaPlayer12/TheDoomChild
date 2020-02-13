d4a2b3ff26d5c06c9bcb4c65f787f36d8a1e7a67
HolySoft_Ayan <betaplayer12@gmail.com>
2020-02-13 23:30:51 +0800

    [!] Fixed Scene 1 Location Poster

--- Assets/DChild/Scripts/Gameplay/System/Zone/LocationData.cs
+++ Assets/DChild/Scripts/Gameplay/System/Zone/LocationData.cs
@@ -4,6 +4,7 @@ using Holysoft.Event;
 using Sirenix.Utilities;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Sirenix.OdinInspector;
 
 namespace DChild.Gameplay.Systems.Serialization
 {
@@ -22,7 +23,7 @@ namespace DChild.Gameplay.Systems.Serialization
     [CreateAssetMenu(fileName = "LocationData", menuName = "DChild/Gameplay/Location Data")]
     public class LocationData : ScriptableObject
     {
-        [SerializeField]
+        [SerializeField, InfoBox("NEVER DUPLICATE THIS!!! THIS WILL CAUSE A CRACK IN SPACE TIME", InfoMessageType.Warning)]
         private Location m_location;
         [SerializeField]
         private SceneInfo m_scene;
@@ -37,12 +38,8 @@ namespace DChild.Gameplay.Systems.Serialization
 #if UNITY_EDITOR
         public void Set(Scene scene, Vector2 position)
         {
-            Debug.Log(scene);
-            //Keeps resetting to random value.... Why???
-            //m_scene.Set(scene);
+            m_scene.Set(scene);
             m_position = position;
-            Debug.LogError("Is Being Set", this);
-            throw new System.Exception("Test Message");
         }
 #endif
 
