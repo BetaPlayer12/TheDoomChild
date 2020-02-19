7f4319abdf7087faf90e401b48edc7360df7e702
LanceNapoleonCruz <lancenapoleon.cruz@gmail.com>
2020-02-19 20:40:36 +0800

    Updatre

--- Assets/DChild/Scripts/Gameplay/System/PlayerSystems/LootHandler/LootHandler.cs
+++ Assets/DChild/Scripts/Gameplay/System/PlayerSystems/LootHandler/LootHandler.cs
@@ -61,7 +61,7 @@ namespace DChild.Gameplay.Systems
             for (int i = 0; i < instanceToCreate; i++)
             {
                 m_cachedLoot = pool.GetOrCreateItem(request.loot).GetComponent<Loot>();
-                SceneManager.MoveGameObjectToScene(m_cachedLoot,)
+                SceneManager.MoveGameObjectToScene(m_cachedLoot.gameObject, request.sceneToDropLoot);
                 m_cachedLoot.SpawnAt(request.location, Quaternion.identity);
                 m_cachedLoot.Pop(GetRandomPopVelocity());
             }
