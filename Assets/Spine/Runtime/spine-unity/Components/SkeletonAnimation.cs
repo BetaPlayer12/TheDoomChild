8b8b3b7f88e3bf29743856e68fb9d911df3b2ba9
HolySoft_Ayan <betaplayer12@gmail.com>
2020-02-27 11:24:40 +0800

    [+] Improving Spine Multithread

--- Assets/Spine/Runtime/spine-unity/Components/SkeletonAnimation.cs
+++ Assets/Spine/Runtime/spine-unity/Components/SkeletonAnimation.cs
@@ -143,6 +143,8 @@ namespace Spine.Unity {
 
 		private bool m_isVisible;
 
+		public bool isVisible { get; }
+
 		/// <summary>
 		/// Clears the previously generated mesh, resets the skeleton's pose, and clears all previously active animations.</summary>
 		public override void ClearState () {
@@ -250,10 +252,10 @@ namespace Spine.Unity {
 
 		public override void LateUpdate()
 		{
-			if (m_alwaysUpdateMesh || m_isVisible)
-			{
+			//if (m_alwaysUpdateMesh || m_isVisible)
+			//{
 				base.LateUpdate();
-			}
+			//}
 		}
 
 		private void OnBecameInvisible()
