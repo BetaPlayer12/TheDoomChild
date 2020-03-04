4165f113d32df3df8eb6457f743f6899a2b2c712
LanceNapoleonCruz <lancenapoleon.cruz@gmail.com>
2020-03-04 11:52:49 +0800

    Update that hopefully fixes everything

--- Assets/Player/Scripts/Behavior/PlotformDrop.cs
+++ Assets/Player/Scripts/Behavior/PlotformDrop.cs
@@ -12,19 +12,16 @@ namespace PlayerNew
         private Collider2D gameObjectCollider;
         public float enableCollider = 0.3f;
 
-
-        // Start is called before the first frame update
-        void Start()
-        {
-
-        }
-
-        // Update is called once per frame
         private void OnCollisionEnter2D(Collision2D collision)
         {
             var down = inputState.GetButtonValue(inputButtons[0]);
             var downhHold = inputState.GetButtonHoldTime(inputButtons[0]);
             var jump = inputState.GetButtonValue(inputButtons[1]);
+
+            if (collision.gameObject.tag == "Droppable")
+            {
+                Debug.Log("Test");
+            }
 
             if (collision.gameObject.tag == "Droppable" && down && jump && downhHold < 0.1f)
             {
@@ -34,12 +31,10 @@ namespace PlayerNew
             }
         }
     
-    
         IEnumerator EnableColliderRoutine()
         {
             yield return new WaitForSeconds(enableCollider);
             collider2D.isTrigger = false;
         }
     }
-
 }
