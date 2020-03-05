94d88070a427772d3ae6d9fe9114abcd60ee188b
Jhaypee Pontz <johnpaulpontino@gmail.com>
2020-03-06 07:48:19 +0800

    Update Player

--- Assets/Player/Scripts/Behavior/StepUp.cs
+++ Assets/Player/Scripts/Behavior/StepUp.cs
@@ -7,10 +7,20 @@ namespace PlayerNew
 {
     public class StepUp : PlayerBehaviour
     {
+        private FaceDirection facing;
+        public Vector2 ledgeBotPos;
+        private Vector2 ledgePos1;
+        private Vector2 ledgePos2;
+
+        public float ledgeClimbXOffset1 = 0f;
+        public float ledgeClimbYOffset1 = 0f;
+        public float ledgeClimbXOffset2 = 0f;
+        public float ledgeClimbYOffset2 = 0f;
+
         // Start is called before the first frame update
         void Start()
         {
-
+            facing = GetComponent<FaceDirection>();
         }
 
         // Update is called once per frame
@@ -20,9 +30,27 @@ namespace PlayerNew
             var right = inputState.GetButtonValue(inputButtons[1]);
 
 
-            if(collisionState.onWallLeg && collisionState.grounded && !collisionState.onWall && !collisionState.isTouchingLedge && (left || right))
+            if(collisionState.onWallLeg && collisionState.grounded && !collisionState.onWall && !collisionState.isTouchingLedge )
             {
-                Debug.Log("Step up");
+                ledgeBotPos = transform.position;
+                if (left)
+                {
+                    float terminalPosX = Mathf.Floor(ledgeBotPos.x - collisionState.rightPosition.x) + ledgeClimbXOffset1;
+                    float terminalPosY = Mathf.Floor(ledgeBotPos.y) + ledgeClimbYOffset1;
+                    ledgePos2 = new Vector2(terminalPosX, terminalPosY);
+
+                }
+                else if (right)
+                {
+                    float terminalPosX = Mathf.Floor(ledgeBotPos.x + collisionState.rightPosition.x) - ledgeClimbXOffset1;
+                    float terminalPosY = Mathf.Floor(ledgeBotPos.y) + ledgeClimbYOffset1;
+
+                    ledgePos2 = new Vector2(terminalPosX, terminalPosY);
+                }
+
+                transform.position = ledgePos2;
+                Debug.Log(ledgePos2);
+
             }
         }
     }
