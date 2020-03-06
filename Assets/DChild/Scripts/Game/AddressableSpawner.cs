4877821dc7e8332e41f5547820b162b753593343
HolySoft_Ayan <betaplayer12@gmail.com>
2020-03-06 11:35:52 +0800

    new Files

---
+++ Assets/DChild/Scripts/Game/AddressableSpawner.cs
@@ -0,0 +1,166 @@
+﻿using DChild.Gameplay;
+using Sirenix.Utilities;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+namespace DChild
+{
+    public static class AddressableSpawner
+    {
+        private struct Request
+        {
+            public Request(Vector3 position, int index, bool hasCallback)
+            {
+                this.position = position;
+                this.index = index;
+                this.hasCallback = hasCallback;
+            }
+
+            public Vector3 position { get; }
+            public int index { get; }
+            public bool hasCallback { get; }
+        }
+
+        private static Dictionary<AssetReferenceT<GameObject>, AsyncOperationHandle<GameObject>> m_loadedAssets = new Dictionary<AssetReferenceT<GameObject>, AsyncOperationHandle<GameObject>>();
+        private static Dictionary<AssetReferenceT<GameObject>, Cache<List<GameObject>>> m_spawnedInstances = new Dictionary<AssetReferenceT<GameObject>, Cache<List<GameObject>>>();
+        private static Dictionary<AssetReferenceT<GameObject>, Cache<Queue<Request>>> m_spawnRequests = new Dictionary<AssetReferenceT<GameObject>, Cache<Queue<Request>>>();
+        private static Dictionary<AssetReferenceT<GameObject>, Cache<Queue<Action<GameObject, int>>>> m_spawnRequestsCallback = new Dictionary<AssetReferenceT<GameObject>, Cache<Queue<Action<GameObject, int>>>>();
+        private static Dictionary<AssetReferenceT<GameObject>, int> m_toBeInstantiatedCount = new Dictionary<AssetReferenceT<GameObject>, int>();
+        public static event Action<GameObject> OnSpawn;
+
+        public static void Spawn(AssetReferenceT<GameObject> asset, Vector3 position, int index, Action<GameObject, int> CallBack = null)
+        {
+
+            var request = new Request(position, index, CallBack != null);
+            if (m_loadedAssets.ContainsKey(asset))
+            {
+                if (m_loadedAssets[asset].IsDone)
+                {
+                    SpawnInstance(asset, request, CallBack);
+                }
+                else
+                {
+                    if (m_spawnRequests.ContainsKey(asset) == false)
+                    {
+                        CreateRequestFor(asset, request, CallBack);
+                    }
+                    else
+                    {
+                        m_toBeInstantiatedCount[asset]++;
+                        m_spawnRequests[asset].Value.Enqueue(request);
+                        if (CallBack != null)
+                        {
+                            m_spawnRequestsCallback[asset].Value.Enqueue(CallBack);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                LoadAndSpawn(asset, request, CallBack);
+            }
+        }
+
+        private static void LoadAndSpawn(AssetReferenceT<GameObject> asset, Request request, Action<GameObject, int> CallBack)
+        {
+            var handle = asset.LoadAssetAsync<GameObject>();
+            m_loadedAssets.Add(asset, handle);
+
+            var cacheSpawnList = Cache<List<GameObject>>.Claim();
+            cacheSpawnList.Value.Clear();
+            m_spawnedInstances.Add(asset, cacheSpawnList);
+
+            CreateRequestFor(asset, request, CallBack);
+
+            handle.Completed += (operation) =>
+            {
+                SpawnFromRequests(asset);
+            };
+        }
+
+        private static void CreateRequestFor(AssetReferenceT<GameObject> asset, Request request, Action<GameObject, int> CallBack)
+        {
+            if (m_toBeInstantiatedCount.ContainsKey(asset) == false)
+            {
+                m_toBeInstantiatedCount.Add(asset, 1);
+            }
+
+            if (m_spawnRequests.ContainsKey(asset) == false)
+            {
+                var cacheRequest = Cache<Queue<Request>>.Claim();
+                cacheRequest.Value.Clear();
+                m_spawnRequests.Add(asset, cacheRequest);
+            }
+            m_spawnRequests[asset].Value.Enqueue(request);
+
+            if (CallBack != null)
+            {
+                if (m_spawnRequestsCallback.ContainsKey(asset) == false)
+                {
+                    var cacheCallBack = Cache<Queue<Action<GameObject, int>>>.Claim();
+                    cacheCallBack.Value.Clear();
+                    m_spawnRequestsCallback.Add(asset, cacheCallBack);
+                }
+                m_spawnRequestsCallback[asset].Value.Enqueue(CallBack);
+            }
+        }
+
+        private static void SpawnFromRequests(AssetReferenceT<GameObject> asset)
+        {
+            var cacheRequest = m_spawnRequests[asset];
+            do
+            {
+                var request = cacheRequest.Value.Dequeue();
+                SpawnInstance(asset, request, request.hasCallback ? m_spawnRequestsCallback[asset].Value.Dequeue() : null);
+            } while (cacheRequest.Value.Count > 0);
+            m_spawnRequests[asset].Value.Clear();
+            m_spawnRequests[asset].Release();
+            m_spawnRequests.Remove(asset);
+            if (m_spawnRequestsCallback.ContainsKey(asset))
+            {
+                m_spawnRequestsCallback[asset].Value.Clear();
+                m_spawnRequestsCallback[asset].Release();
+                m_spawnRequestsCallback.Remove(asset);
+            }
+        }
+
+        private static void SpawnInstance(AssetReferenceT<GameObject> asset, Request request, Action<GameObject, int> CallBack)
+        {
+            m_toBeInstantiatedCount[asset]++;
+            asset.InstantiateAsync(request.position, Quaternion.identity).Completed += (operation) =>
+            {
+                m_spawnedInstances[asset].Value.Add(operation.Result);
+                //var instance = operation.Result.AddComponent<AddressableInstance>();
+                //instance.reference = asset;
+                //instance.OnDestroyInstance = OnInstanceDestroyed;
+                m_toBeInstantiatedCount[asset]--;
+
+                if (request.hasCallback)
+                {
+                    CallBack?.Invoke(operation.Result, request.index);
+                }
+                OnSpawn?.Invoke(operation.Result);
+            };
+        }
+
+        private static void OnInstanceDestroyed(AssetReferenceT<GameObject> asset, GameObject instance)
+        {
+            m_spawnedInstances[asset].Value.Remove(instance);
+            if (m_spawnedInstances[asset].Value.Count > 0 && m_toBeInstantiatedCount[asset] == 0)
+            {
+                m_spawnedInstances[asset].Release();
+                m_spawnedInstances.Remove(asset);
+                m_toBeInstantiatedCount.Remove(asset);
+
+                Addressables.Release(m_loadedAssets[asset]);
+                asset.ReleaseAsset();
+                m_loadedAssets.Remove(asset);
+            }
+        }
+    }
+
+}
---
+++ Assets/DChild/Scripts/Gameplay/System/QuickItem/QuickItemHandle.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using DChild.Gameplay.Characters.Players;
+using DChild.Gameplay.Items;
+using Holysoft.Event;
+using Sirenix.OdinInspector;
+using Sirenix.Utilities;
+using UnityEngine;
+
+namespace DChild.Gameplay.Inventories
+{
+    public class QuickItemHandle : SerializedMonoBehaviour
+    {
+        public class SelectionEventArgs : IEventActionArgs
+        {
+            public enum SelectionType
+            {
+                Next,
+                Previous,
+                None
+            }
+
+            public int currentIndex { get; private set; }
+            public ItemSlot currentSlot { get; private set; }
+            public SelectionType selectionType { get; private set; }
+
+            public void Initialize(int currentIndex, ItemSlot currentSlot, SelectionType selectionType)
+            {
+                this.currentIndex = currentIndex;
+                this.currentSlot = currentSlot;
+                this.selectionType = selectionType;
+            }
+        }
+
+        [SerializeField]
+        private IPlayer m_player;
+        [SerializeField]
+        private bool m_wrapped;
+        [SerializeField]
+        private IItemContainer m_container;
+
+        [ShowInInspector, ReadOnly]
+        private int m_currentIndex;
+        private ItemSlot m_currentSlot;
+        public event EventAction<SelectionEventArgs> SelectedItem;
+        public event EventAction<SelectionEventArgs> Update;
+
+        public bool isWrapped => m_wrapped;
+        public int currentIndex => m_currentIndex;
+        public IItemContainer container => m_container;
+
+        public void UseCurrentItem()
+        {
+            m_currentSlot = m_container.GetSlot(m_currentIndex);
+            if (m_player != null)
+            {
+                ((UsableItemData)m_currentSlot.item).Use(m_player);
+            }
+            m_container.AddItem(m_currentSlot.item, -1);
+        }
+
+        [Button, HorizontalGroup("Split"), HideInEditorMode]
+        public void Previous()
+        {
+            if (m_currentIndex == 0)
+            {
+                if (m_wrapped)
+                {
+                    m_currentIndex = m_container.Count - 1;
+                    StoreSelectedItem(SelectionEventArgs.SelectionType.Previous);
+                }
+                else
+                {
+                    StoreSelectedItem(SelectionEventArgs.SelectionType.None);
+                }
+            }
+            else
+            {
+                m_currentIndex--;
+                StoreSelectedItem(SelectionEventArgs.SelectionType.Previous);
+            }
+        }
+
+        [Button, HorizontalGroup("Split"), HideInEditorMode]
+        public void Next()
+        {
+            if (m_currentIndex == m_container.Count - 1)
+            {
+                if (m_wrapped)
+                {
+                    m_currentIndex = 0;
+                    StoreSelectedItem(SelectionEventArgs.SelectionType.Next);
+                }
+                else
+                {
+                    StoreSelectedItem(SelectionEventArgs.SelectionType.None);
+                }
+            }
+            else
+            {
+                m_currentIndex++;
+                StoreSelectedItem(SelectionEventArgs.SelectionType.Next);
+            }
+        }
+
+        private void StoreSelectedItem(SelectionEventArgs.SelectionType selectionType)
+        {
+            m_currentSlot = m_container.GetSlot(m_currentIndex);
+            using (Cache<SelectionEventArgs> cacheEventArgs = Cache<SelectionEventArgs>.Claim())
+            {
+                cacheEventArgs.Value.Initialize(m_currentIndex, m_currentSlot, selectionType);
+                SelectedItem?.Invoke(this, cacheEventArgs.Value);
+                cacheEventArgs.Release();
+            }
+        }
+
+        private void OnItemUpdate(object sender, ItemEventArgs eventArgs)
+        {
+            if (eventArgs.count == 0 && eventArgs.data == m_currentSlot.item)
+            {
+                Previous();
+            }
+        }
+
+        private void Awake()
+        {
+            m_currentIndex = 0;
+            m_currentSlot = m_container.GetSlot(m_currentIndex);
+            m_container.ItemUpdate += OnItemUpdate;
+        }
+    }
+}
