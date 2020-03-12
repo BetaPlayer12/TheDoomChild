fb13f1842a710a8f74fb31b7653fb2884993fd95
LanceNapoleonCruz <lancenapoleon.cruz@gmail.com>
2020-03-12 14:06:36 +0800

    Added OrderLayerHandle

---
+++ Assets/DChild/Scripts/DevelopmentOnly/OrderLayerHandle.cs
@@ -0,0 +1,44 @@
+﻿using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrderLayerHandle : MonoBehaviour
+{
+    [SerializeField, OnValueChanged("UpdateSortingOrder")]
+    private int m_referenceOrder;
+
+#if UNITY_EDITOR
+
+    private void UpdateSortingOrder()
+    {
+        var parentObjectRenderer = GetComponent<Renderer>();
+
+        if(parentObjectRenderer != null)
+        {
+            parentObjectRenderer.sortingOrder += m_referenceOrder;
+
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                var child = transform.GetChild(i);
+
+                if (child != null)
+                {
+                    var childRenderer = child.GetComponent<Renderer>();
+
+                    if (childRenderer != null)
+                    {
+                        childRenderer.sortingOrder += m_referenceOrder;
+                    }
+                }
+            }
+        }
+    }
+
+    [Button("Reset")]
+    private void ResetOrderLayer()
+    {
+         
+    }
+#endif
+}
