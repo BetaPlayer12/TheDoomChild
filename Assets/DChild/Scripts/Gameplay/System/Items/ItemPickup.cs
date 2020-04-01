86a8e971f20d8680ad7380f0154fe2bee776f22c
HolySoft_Ayan <betaplayer12@gmail.com>
2020-04-01 15:13:43 +0800

    [+] Created GenericKey Loot

--- Assets/DChild/Scripts/Gameplay/System/Items/ItemPickup.cs
+++ Assets/DChild/Scripts/Gameplay/System/Items/ItemPickup.cs
@@ -1,18 +1,46 @@
-﻿using System.Collections;
+﻿using DChild.Gameplay.Characters.Players;
+using DChild.Gameplay.Environment.Interractables;
+using DChild.Serialization;
+using Sirenix.OdinInspector;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemPickup : MonoBehaviour
+namespace DChild.Gameplay.Items
 {
-    // Start is called before the first frame update
-    void Start()
+    public class ItemPickup : MonoBehaviour, IButtonToInteract, ISerializableComponent
     {
-        
-    }
+        [System.Serializable]
+        public struct SaveData : ISaveData
+        {
+            [SerializeField]
+            private bool m_isPickedUp;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            public SaveData(bool isPickedUp)
+            {
+                m_isPickedUp = isPickedUp;
+            }
+
+            public bool isPickedUp => m_isPickedUp;
+        }
+
+        [SerializeField]
+        private Transform m_promptPostion;
+        [SerializeField]
+        private ItemData m_data;
+
+        public bool showPrompt => true;
+
+        public Vector3 promptPosition => m_promptPostion.position;
+
+        public void Interact(Character character)
+        {
+            character.GetComponent<PlayerControlledObject>().owner.inventory.AddItem(m_data);
+            gameObject.SetActive(false);
+        }
+
+        public ISaveData Save() => new SaveData(!gameObject.activeSelf);
+
+        public void Load(ISaveData data) => gameObject.SetActive(((SaveData)data).isPickedUp == false);
     }
 }
