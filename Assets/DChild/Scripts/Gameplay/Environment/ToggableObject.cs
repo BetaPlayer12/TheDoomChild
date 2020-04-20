56e8033ee61e46ba858291901ad17952a6e31bb8
HolySoft_Ayan <betaplayer12@gmail.com>
2020-04-20 18:10:45 +0800

    [+] Created ToggableObject for 2State Object Thingy

---
+++ Assets/DChild/Scripts/Gameplay/Environment/ToggableObject.cs
@@ -0,0 +1,37 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace DChild.Gameplay.Environment
+{
+    public class ToggableObject : MonoBehaviour
+    {
+        [SerializeField,HideInPlayMode]
+        private bool m_startAs;
+        [ShowInInspector,HideInEditorMode]
+        private bool m_currentState;
+
+        [SerializeField,TabGroup("True")]
+        private UnityEvent m_onTrue;
+        [SerializeField, TabGroup("False")]
+        private UnityEvent m_onFalse;
+
+        public void SetToggleState(bool value)
+        {
+            m_currentState = value;
+            if (m_currentState)
+            {
+                m_onTrue?.Invoke();
+            }
+            else
+            {
+                m_onTrue?.Invoke();
+            }
+        }
+
+        private void Start()
+        {
+            SetToggleState(m_startAs);  
+        }
+    }
+}
