b8a681a5494e5e133cc80826a81614ca46d00c36
HolySoft_Ayan <betaplayer12@gmail.com>
2020-05-22 14:57:53 +0800

    [+] Added Waterfalls for the garden

---
+++ Assets/DChild/Scripts/Gameplay/Environment/WaterWheelBlocker.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace DChild.Gameplay.Environment
+{
+    public class WaterWheelBlocker : MonoBehaviour
+    {
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.TryGetComponentInParent(out WaterfallWheelHandle handle))
+            {
+                handle.BlockWater();
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            if (collision.TryGetComponentInParent(out WaterfallWheelHandle handle))
+            {
+                handle.UnblockWater();
+            }
+        }
+    }
+}
