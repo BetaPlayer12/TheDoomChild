d3d17282143374b30b5604136bd53b4c1ef130b8
Adrianne Joseph Chua <chua.adrianne@gmail.com>
2020-07-24 18:43:02 +0800

    [+] aasddasdasd

--- Assets/DChild/Scripts/Gameplay/UI/RoomText.cs
+++ Assets/DChild/Scripts/Gameplay/UI/RoomText.cs
@@ -1,68 +1,69 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
-using DChild.Gameplay.Characters.AI;
 using DChild.Gameplay.Combat;
 using DChild.Serialization;
-using Holysoft.Event;
 using Sirenix.OdinInspector;
 using UnityEngine;
 using Doozy.Engine.UI;
 
-public class RoomText : MonoBehaviour, ISerializableComponent
+namespace DChild.Gameplay.UI
 {
-    public struct SaveData : ISaveData
+    public class RoomText : MonoBehaviour, ISerializableComponent
     {
-        [SerializeField]
-        private bool m_Shown;
-
-
-
-        public SaveData(bool m_Shown)
+        public struct SaveData : ISaveData
         {
-            this.m_Shown = m_Shown;
+            [SerializeField]
+            private bool m_Shown;
+
+
+
+            public SaveData(bool m_Shown)
+            {
+                this.m_Shown = m_Shown;
+            }
+
+            public bool Shown => m_Shown;
         }
 
-        public bool Shown => m_Shown;
-    }
+        [SerializeField]
+        private bool m_Shown;
+        [SerializeField]
+        private UIView m_UI;
+        [SerializeField, MinValue(0)]
+        private float m_TextFadeDelay;
 
-    [SerializeField]
-    private bool m_Shown;
-    [SerializeField]
-    private UIView m_UI;
-    [SerializeField, MinValue(0)]
-    private float m_TextFadeDelay;
-
-    public ISaveData Save()
-    {
-        return new SaveData(m_Shown);
-    }
-
-    public void Load(ISaveData data)
-    {
-        var saveData = (SaveData)data;
-        m_Shown = saveData.Shown;
-
-    }
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (m_Shown != true)
+        public ISaveData Save()
         {
-            if (collision.isTrigger)
+            return new SaveData(m_Shown);
+        }
+
+        public void Load(ISaveData data)
+        {
+            var saveData = (SaveData)data;
+            m_Shown = saveData.Shown;
+
+        }
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (m_Shown != true)
             {
-                if (collision.TryGetComponent(out Hitbox check))
+                if (collision.isTrigger)
                 {
-                    //show animation
-                    m_UI.Show();
-                    m_Shown = true;
-                    StartCoroutine(DelayedFade());
+                    if (collision.TryGetComponent(out Hitbox check))
+                    {
+                        //show animation
+                        m_UI.Show();
+                        m_Shown = true;
+                        StartCoroutine(DelayedFade());
+                    }
                 }
             }
         }
+
+        private IEnumerator DelayedFade()
+        {
+            yield return new WaitForSeconds(m_TextFadeDelay);
+            m_UI.Hide();
+        }
     }
 
-    private IEnumerator DelayedFade()
-    {
-        yield return new WaitForSeconds(m_TextFadeDelay);
-        m_UI.Hide();
-    }
 }
