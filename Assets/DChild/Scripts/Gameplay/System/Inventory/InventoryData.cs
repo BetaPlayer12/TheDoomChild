91d19aa5f9d924eb536004b74bf499c3e46e5ee8
Ayan Summ J. Castillano <betaplayer12@gmail.com>
2020-09-30 14:03:14 +0800

    [*] Merchant can now have infinite stocks

--- Assets/DChild/Scripts/Gameplay/System/Inventory/InventoryData.cs
+++ Assets/DChild/Scripts/Gameplay/System/Inventory/InventoryData.cs
@@ -1,4 +1,5 @@
 ﻿using DChild.Gameplay.Items;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace DChild.Gameplay.Inventories
@@ -11,18 +12,43 @@ namespace DChild.Gameplay.Inventories
         {
             [SerializeField]
             private ItemData m_item;
-            [SerializeField]
+            [SerializeField, OnValueChanged("OnLimitCountChange")]
+            private bool m_hasLimitedCount;
+            [SerializeField, EnableIf("m_hasLimitedCount")]
             private int m_count;
 
             public ItemData item => m_item;
+            public bool hasLimitedCount => m_hasLimitedCount;
             public int count => m_count;
+
+#if UNITY_EDITOR
+            private void OnLimitCountChange()
+            {
+                if (m_hasLimitedCount == false)
+                {
+                    m_count = 99;
+                }
+            }
+#endif
         }
 
-        [SerializeField]
+        [SerializeField,TableList(AlwaysExpanded = true)]
         private Slot[] m_slots;
 
         public int slotCount => m_slots.Length;
 
         public (ItemData item, int count) GetInfo(int index) => (m_slots[index].item, m_slots[index].count);
+
+        public bool HasLimitedCount(ItemData itemData)
+        {
+            for (int i = 0; i < m_slots.Length; i++)
+            {
+                if (m_slots[i].item == itemData)
+                {
+                    return m_slots[i].hasLimitedCount;
+                }
+            }
+            return false;
+        }
     }
 }
