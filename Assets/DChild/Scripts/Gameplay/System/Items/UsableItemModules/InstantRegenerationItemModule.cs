fed9263bb19d454ca052eec1cba69985461fdede
Ayan Summ J. Castillano <betaplayer12@gmail.com>
2020-10-02 16:05:52 +0800

    [+] Created Functionality for Rage Potion to work [+] Summary for Items created for easy check of modules

--- Assets/DChild/Scripts/Gameplay/System/Items/UsableItemModules/InstantRegenerationItemModule.cs
+++ Assets/DChild/Scripts/Gameplay/System/Items/UsableItemModules/InstantRegenerationItemModule.cs
@@ -1,11 +1,17 @@
 ﻿using DChild.Gameplay.Characters.Players;
+using DChild.Gameplay.Combat;
+using Holysoft.Event;
 using Sirenix.OdinInspector;
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace DChild.Gameplay.Items
 {
+
     [System.Serializable]
-    public struct RegenerationItemModule : IUsableItemModule
+    public struct InstantRegenerationItemModule : IUsableItemModule
     {
         private enum Stat
         {
@@ -45,6 +51,18 @@ namespace DChild.Gameplay.Items
             else
             {
                 return player.magic.isFull == false;
+            }
+        }
+
+        public override string ToString()
+        {
+            if (m_value >= 0)
+            {
+                return $"Regenerate {m_value} {m_toRegenerate.ToString()}";
+            }
+            else
+            {
+                return $"Consumes {-m_value} {m_toRegenerate.ToString()}";
             }
         }
     }
