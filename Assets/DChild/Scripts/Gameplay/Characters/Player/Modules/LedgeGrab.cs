d39e2696cf76bc37da53bb11d510ff1cd8cfcf26
Ayan Summ J. Castillano <betaplayer12@gmail.com>
2020-11-06 15:24:33 +0800

    [*]Ledge Grab setup for intergration

--- Assets/DChild/Scripts/Gameplay/Characters/Player/Modules/LedgeGrab.cs
+++ Assets/DChild/Scripts/Gameplay/Characters/Player/Modules/LedgeGrab.cs
@@ -1,4 +1,5 @@
 ﻿using DChild.Gameplay.Characters.Players.State;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace DChild.Gameplay.Characters.Players.Modules
@@ -6,14 +7,17 @@ namespace DChild.Gameplay.Characters.Players.Modules
     public class LedgeGrab : MonoBehaviour, IComplexCharacterModule
     {
         [SerializeField]
-        private RaySensor m_wallSensor;
+        private RaySensor m_grabbableWallSensor;
         [SerializeField]
         private RaySensor m_overheadSensor;
         [SerializeField]
         private RaySensor m_destinationSensor;
+        [SerializeField, MinValue(0f)]
+        private float m_destinationFromWallOffset;
 
         private int m_animation;
         private Character m_character;
+        private Rigidbody2D m_rigidbody;
         private Animator m_animator;
         private ILedgeGrabState m_state;
         private Vector2 m_destination;
@@ -21,17 +25,24 @@ namespace DChild.Gameplay.Characters.Players.Modules
         public void Initialize(ComplexCharacterInfo info)
         {
             m_character = info.character;
-            //m_state = info.state;
+            m_rigidbody = info.rigidbody;
+            m_state = info.state;
+            m_animator = info.animator;
+            m_animation = info.animationParametersData.GetParameterLabel(AnimationParametersData.Parameter.LedgeGrab);
         }
 
         public bool IsDoable()
         {
-            m_wallSensor.Cast();
-            if (m_wallSensor.isDetecting)
+            m_grabbableWallSensor.Cast();
+            if (m_grabbableWallSensor.allRaysDetecting)
             {
                 m_overheadSensor.Cast();
                 if (m_overheadSensor.isDetecting == false)
                 {
+                    var wallPoint = m_grabbableWallSensor.GetValidHits()[0].point;
+                    var destinationPosition = m_destinationSensor.transform.position;
+                    destinationPosition.x = wallPoint.x + (m_destinationFromWallOffset * (int)m_character.facing);
+                    m_destinationSensor.transform.position = destinationPosition;
                     m_destinationSensor.Cast();
                     if (m_destinationSensor.isDetecting)
                     {
@@ -45,7 +56,8 @@ namespace DChild.Gameplay.Characters.Players.Modules
 
         public void Execute()
         {
-            m_character.transform.position = m_destination;
+            m_rigidbody.position = m_destination;
+            m_rigidbody.velocity = Vector2.zero;
             m_animator.SetTrigger(m_animation);
             m_state.waitForBehaviour = true;
         }
