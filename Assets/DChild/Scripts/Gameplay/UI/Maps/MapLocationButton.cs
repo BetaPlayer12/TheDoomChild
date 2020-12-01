d15636f5745e3a7977310c19c12d400c00a9256e
Ayan Summ J. Castillano <betaplayer12@gmail.com>
2020-12-01 13:52:21 +0800

    [+] World Map Location Picker Setup

---
+++ Assets/DChild/Scripts/Gameplay/UI/Maps/MapLocationButton.cs
@@ -0,0 +1,26 @@
+﻿using DChild.Gameplay.Systems.Serialization;
+using Doozy.Engine;
+using Holysoft.Event;
+using System;
+using UnityEngine;
+
+namespace DChild.Gameplay.UI.Map
+{
+    public class MapLocationButton : MonoBehaviour
+    {
+        [SerializeField]
+        private LocationData m_location;
+
+        public void GoHere()
+        {
+            GameSystem.RequestConfirmation(OnAccept, $"Travel to {m_location.location.ToString()}");
+        }
+
+        private void OnAccept(object sender, EventActionArgs eventArgs)
+        {
+            GameplaySystem.playerManager.player.character.transform.position = m_location.position;
+            GameSystem.LoadZone(m_location.scene,true);
+            GameEventMessage.SendEvent("Location Transfer");
+        }
+    }
+}
