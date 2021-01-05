492f192243c0c9423df9b93f5e4361a27572d72a
Ayan Summ J. Castillano <betaplayer12@gmail.com>
2021-01-05 16:55:38 +0800

    [+] Location Picker Upload

---
+++ Assets/Debug/LocationPicker.cs
@@ -0,0 +1,17 @@
+﻿using DChild.Gameplay;
+using Doozy.Engine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DChild.Gameplay
+{
+    public class LocationPicker : MonoBehaviour
+    {
+        void Start()
+        {
+            GameplaySystem.PauseGame();
+            GameEventMessage.SendEvent("Open LocationPicker");
+        }
+    }
+}
