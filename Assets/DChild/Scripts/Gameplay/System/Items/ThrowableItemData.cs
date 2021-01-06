5c740d1487a0cc821ada8f5b385d387b50abc71e
Ayan Summ J. Castillano <betaplayer12@gmail.com>
2021-01-06 17:34:36 +0800

    [*] Skull Throw Modification

--- Assets/DChild/Scripts/Gameplay/System/Items/ThrowableItemData.cs
+++ Assets/DChild/Scripts/Gameplay/System/Items/ThrowableItemData.cs
@@ -1,5 +1,7 @@
-﻿using DChild.Gameplay.Characters.Players;
+﻿using DChild.Gameplay.Characters;
+using DChild.Gameplay.Characters.Players;
 using DChild.Gameplay.Characters.Players.Modules;
+using System.Collections;
 using UnityEngine;
 
 namespace DChild.Gameplay.Items
@@ -8,21 +10,57 @@ namespace DChild.Gameplay.Items
     public class ThrowableItemData : ConsumableItemData
     {
         [SerializeField]
-        private GameObject m_projectile;
+        private ProjectileInfo m_projectile;
+        private static bool m_isOnCooldown;
+        private static bool m_isInUse;
 
         public override bool CanBeUse(IPlayer player)
         {
-            return true;
+            return m_isOnCooldown == false && player.state.isAttacking == false;
         }
 
         public override void Use(IPlayer player)
         {
-            ////var handle = player.character.GetComponent<ProjectileThrowHandler>();
-            ////handle.SetProjectile(m_projectile);
-            ////handle.Initialize();
+            if (m_isInUse == false)
+            {
+                var handle = player.character.GetComponent<SkullThrow>();
+                handle.SetProjectileInfo(m_projectile);
+                if (player.state.isGrounded)
+                {
+                    handle.StartAim();
+                    handle.Execute();
+                    player.character.StartCoroutine(TrackInputRoutine(player, handle));
+                }
+                else
+                {
+                    handle.Execute();
+                    handle.StartThrow();
+                }
+            }
+
             /*Start Throw via Controller
              Controller decides when to spawn the projectile
              this only starts the process*/
+        }
+
+        private IEnumerator TrackInputRoutine(IPlayer player, SkullThrow handle)
+        {
+            bool keepInLoop = true;
+            while (keepInLoop)
+            {
+                yield return null;
+                if (player.state.isGrounded)
+                {
+                    if (true) //KeyReleased
+                    {
+                        break;
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
         }
     }
 }
