3f056eda979e39e5bf9ca924c3315d740d7e2335
Adrianne Joseph Chua <chua.adrianne@gmail.com>
2021-01-20 17:49:06 +0800

    UI message

--- Assets/DChild/Scripts/Gameplay/UI/HUD/CurrencyUI.cs
+++ Assets/DChild/Scripts/Gameplay/UI/HUD/CurrencyUI.cs
@@ -23,6 +23,7 @@ namespace DChild.Gameplay.UI
         private bool m_addedAmountIsNegative;
         private float m_delayTimer;
         private bool m_delayAddingOfAmount;
+        private static GameplayUIHandle m_gameplayUIHandle;
 
         private int currentAmount
         {
@@ -77,7 +78,7 @@ namespace DChild.Gameplay.UI
             addedAmount += eventArgs.amount;
             m_delayTimer = m_addAmountDelay;
             m_delayAddingOfAmount = true;
-            GameEventMessage.SendEvent("Soul Essence Added");
+            m_gameplayUIHandle.ShowPromptSoulEssenceChangeNotify();
             enabled = true;
         }
 
@@ -116,7 +117,7 @@ namespace DChild.Gameplay.UI
                 if (addedAmount == 0)
                 {
                     enabled = false;
-                    GameEventMessage.SendEvent("Soul Essence Hide");
+                    m_gameplayUIHandle.ShowSoulEssenceNotify(false);
                 }
             }
         }
