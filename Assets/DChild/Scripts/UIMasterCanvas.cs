cda5d2a0bf20338ecce4c7913bae6fc49bff91c8
Ayan Summ J. Castillano <betaplayer12@gmail.com>
2021-02-15 14:33:20 +0800

    [!] Canvas is Hidden When there is no camera present

--- Assets/DChild/Scripts/UIMasterCanvas.cs
+++ Assets/DChild/Scripts/UIMasterCanvas.cs
@@ -22,6 +22,7 @@ namespace DChild
         private void OnCameraChange(object sender, CameraChangeEventArgs eventArgs)
         {
             m_canvas.worldCamera = eventArgs.camera;
+            m_canvas.enabled = eventArgs.camera != null;
         }
     }
 }
