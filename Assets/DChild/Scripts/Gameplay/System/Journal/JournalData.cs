4d9c8c145b09edf95e321039430173a1c01c7965
Adrianne Joseph Chua <chua.adrianne@gmail.com>
2021-02-24 15:54:57 +0800

    commit journal script

---
+++ Assets/DChild/Scripts/Gameplay/System/Journal/JournalData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DChild.Gameplay.System.Journal
+{
+    public class JournalData : MonoBehaviour
+    {
+        private int m_id;
+        private Sprite m_notification;
+    }
+}
---
+++ Assets/DChild/Scripts/Gameplay/System/Journal/JournalNotification.cs
@@ -0,0 +1,18 @@
+﻿using DChild.Gameplay.Systems;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DChild.Gameplay.System.Journal
+{
+    public class JournalNotification : MonoBehaviour
+    {
+        private GameplayInput m_gameplayinput;
+        private JournalNotificationUI m_ui;
+
+        public void UpdateNotification(JournalData journaldata)
+        {
+
+        }
+    }
+}
