6b1dac532eb550a1d7c3db8158d2e61f641d4fc2
Adrianne Joseph Chua <chua.adrianne@gmail.com>
2021-04-27 13:49:51 +0800

    commit debug

---
+++ Assets/DChild/Scripts/BuildDebug/ToggleInfiniteShadow.cs
@@ -0,0 +1,25 @@
+﻿using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DChildDebug.Window
+{
+    public class ToggleInfiniteShadow : MonoBehaviour
+    {
+
+        [Button]
+        public void ToggleOn()
+        {
+            
+
+        }
+
+        [Button]
+        public void ToggleOff()
+        {
+
+
+        }
+    }
+}
