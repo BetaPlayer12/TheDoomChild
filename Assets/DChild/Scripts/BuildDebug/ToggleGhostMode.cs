64af8dd769ebee70ca1e8c5fd925fb9fd17704f9
Ayan Summ J. Castillano <betaplayer12@gmail.com>
2021-04-30 10:18:14 +0800

    [*] INtergrated Some Debug Window Behaviour to Debug Window UI

--- Assets/DChild/Scripts/BuildDebug/ToggleGhostMode.cs
+++ Assets/DChild/Scripts/BuildDebug/ToggleGhostMode.cs
@@ -5,13 +5,19 @@ using UnityEngine;
 
 namespace DChildDebug.Window
 {
-    public class ToggleGhostMode : MonoBehaviour
+    public class ToggleGhostMode : MonoBehaviour, IToggleDebugBehaviour
     {
         [SerializeField]
         public GameObject m_player;
+
+        private bool m_isOn;
+
+        public bool value => m_isOn;
+
         [Button]
         public void ToggleOn()
         {
+            m_isOn = true;
             m_player.GetComponentInChildren<Collider2D>().enabled = false;
             //m_player.GetComponentInChildren<Rigidbody2D>().gravityScale = 0;
            
@@ -20,6 +26,8 @@ namespace DChildDebug.Window
         [Button]
         public void ToggleOff()
         {
+            m_isOn = false;
+
             m_player.GetComponentInChildren<Collider2D>().enabled = true;
             //m_player.GetComponentInChildren<Rigidbody2D>().gravityScale = 20;
         }
