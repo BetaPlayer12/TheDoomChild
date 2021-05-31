3eff4bfee1618aab0aeb68cafbc50a3314ed2a97
HolySoft_Ayan <betaplayer12@gmail.com>
2021-05-31 14:18:22 +0800

    [+] Seperate Configuration Data for Game Settings [*] Settings available in pause

--- Assets/DChild/Scripts/Settings/AudioSettings.cs
+++ Assets/DChild/Scripts/Settings/AudioSettings.cs
@@ -5,17 +5,29 @@ namespace DChild.Configurations
     [System.Serializable]
     public class AudioSettings
     {
+        [SerializeField]
+        private float m_masterVolume = 1;
+        [SerializeField]
+        private float m_soundVolume = 1;
+        [SerializeField]
+        private float m_musicVolume = 1;
+
+        public float masterVolume
+        {
+            get => m_masterVolume;
+            set => m_masterVolume = value;
+        }
+
+        public float soundVolume
+        {
+            get => m_soundVolume;
+            set => m_soundVolume = value;
+        }
+
         public float musicVolume
         {
-            get
-            {
-                return 0;
-            }
-
-            set
-            {
-
-            }
+            get => m_musicVolume;
+            set => m_musicVolume = value;
         }
 
 
---
+++ Assets/DChild/Scripts/Settings/GameSettingsConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using Sirenix.OdinInspector;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DChild.Configurations
+{
+    [Serializable]
+    public class GameSettingsConfiguration
+    {
+        [Serializable]
+        public struct VisualConfiguration
+        {
+            public int resolutionIndex;
+            public bool fullscreen;
+            public bool vsync;
+            [MinValue(0)]
+            public float brightness;
+            [MinValue(0)]
+            public float contrast;
+        }
+
+        [Serializable]
+        public struct AudioConfiguration
+        {
+            [MinValue(0)]
+            public float masterVolume;
+            [MinValue(0)]
+            public float soundVolume;
+            [MinValue(0)]
+            public float musicVolume;
+        }
+
+        public VisualConfiguration visualConfiguration;
+        public AudioConfiguration audioConfiguration;
+    }
+
+}
