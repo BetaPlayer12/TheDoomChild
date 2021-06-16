fe3bd1922a098ec31df0c10501f608dbbf4b2029
HolySoft_Ayan <betaplayer12@gmail.com>
2021-06-16 15:59:46 +0800

    [*] Skill Ceremony Update

--- Assets/DChild/Scripts/Gameplay/System/Cinematics/CutsceneInitializer/SkillAcquireCutsceneInitializer.cs
+++ Assets/DChild/Scripts/Gameplay/System/Cinematics/CutsceneInitializer/SkillAcquireCutsceneInitializer.cs
@@ -17,14 +17,14 @@ namespace DChild.Gameplay.Cinematics.Cutscenes
         [SerializeField]
         private PlayableAsset m_cinematic;
 
-        [SerializeField,BoxGroup("References")]
-        private CinemachineBrain m_cinemachineBrain;
-        [SerializeField, BoxGroup("References")]
-        private CinemachineVirtualCamera m_sceneCamera;
-        [SerializeField, BoxGroup("References")]
-        private SignalEmitter m_signalEmitter;
+        //[SerializeField,BoxGroup("References")]
+        //private CinemachineBrain m_cinemachineBrain;
+        //[SerializeField, BoxGroup("References")]
+        //private CinemachineVirtualCamera m_sceneCamera;
+        //[SerializeField, BoxGroup("References")]
+        //private SignalEmitter m_signalEmitter;
 
-        private const string TRACKNAME_CUTSCENE = "SceneCamera";
+        //private const string TRACKNAME_CUTSCENE = "SceneCamera";
 
         private void Start()
         {
@@ -41,31 +41,31 @@ namespace DChild.Gameplay.Cinematics.Cutscenes
                         case SpineAnimationStateTrack spineAnimTrack:
                             var playerAnimation = GameplaySystem.playerManager.player.character.GetComponentInChildren<SkeletonAnimation>();
                             m_cutscene.SetGenericBinding(binding.sourceObject, playerAnimation);
-                            break;
-                        case CinemachineTrack cinemachineTrack:
-                            AssignCinemachineTrackData(binding, cinemachineTrack);
-                            break;
-                        case SignalTrack signalTrack:
-                            m_cutscene.SetGenericBinding(binding.sourceObject, m_signalEmitter);
-                            break;
+                            return;
+                        //case CinemachineTrack cinemachineTrack:
+                        //    AssignCinemachineTrackData(binding, cinemachineTrack);
+                        //    break;
+                        //case SignalTrack signalTrack:
+                        //    m_cutscene.SetGenericBinding(binding.sourceObject, m_signalEmitter);
+                        //    break;
                     }
                 }
             }
 
         }
 
-        private void AssignCinemachineTrackData(PlayableBinding binding, CinemachineTrack cinemachineTrack)
-        {
-            m_cutscene.SetGenericBinding(binding.sourceObject, m_cinemachineBrain);
-            var clipList = cinemachineTrack.GetClips();
-            foreach (var clip in clipList)
-            {
-                if (clip.displayName == "SceneCamera")
-                {
-                    var shot = (CinemachineShot)clip.asset;
-                    shot.VirtualCamera.defaultValue = m_sceneCamera;
-                }
-            }
-        }
+        //private void AssignCinemachineTrackData(PlayableBinding binding, CinemachineTrack cinemachineTrack)
+        //{
+        //    m_cutscene.SetGenericBinding(binding.sourceObject, m_cinemachineBrain);
+        //    var clipList = cinemachineTrack.GetClips();
+        //    foreach (var clip in clipList)
+        //    {
+        //        if (clip.displayName == "SceneCamera")
+        //        {
+        //            var shot = (CinemachineShot)clip.asset;
+        //            shot.VirtualCamera.defaultValue = m_sceneCamera;
+        //        }
+        //    }
+        //}
     }
 }
