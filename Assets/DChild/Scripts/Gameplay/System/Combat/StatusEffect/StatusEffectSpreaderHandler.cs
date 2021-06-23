642bee24dba0956e698f32c5e59b22538e907b05
serpentdrake <krateusy@gmail.com>
2021-06-23 13:16:54 +0800

    [*]Modified status effect spreader handler script

--- Assets/DChild/Scripts/Gameplay/System/Combat/StatusEffect/StatusEffectSpreaderHandler.cs
+++ Assets/DChild/Scripts/Gameplay/System/Combat/StatusEffect/StatusEffectSpreaderHandler.cs
@@ -115,6 +115,64 @@ public class StatusEffectSpreaderHandler : MonoBehaviour
 
     }
 
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            var otherGameObject = collision.gameObject;
+
+            if (otherGameObject.tag == "Hitbox")
+            {
+                var characterStatusEffectReciever = otherGameObject.GetComponentInParent<StatusEffectReciever>();
+
+                if (characterStatusEffectReciever)
+                {
+                    foreach (var i in statusEffectList)
+                    {
+                        if (characterStatusEffectReciever.IsInflictedWith(i.Key.type))
+                        {
+                            characterStatusEffectReciever.ResetDuration(i.Key.type);
+
+                        }
+                        else
+                        {
+                            GameplaySystem.combatManager.Inflict(characterStatusEffectReciever, i.Key.type);
+                        }
+                    }
+
+
+                }
+            }
+        }
+        else if (collision.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+        {
+            var otherGameObject = collision.gameObject;
+
+            if (otherGameObject.tag == "Hitbox")
+            {
+                var characterStatusEffectReciever = otherGameObject.GetComponentInParent<StatusEffectReciever>();
+
+                if (characterStatusEffectReciever)
+                {
+                    foreach (var i in statusEffectList)
+                    {
+                        if (characterStatusEffectReciever.IsInflictedWith(i.Key.type))
+                        {
+                            characterStatusEffectReciever.ResetDuration(i.Key.type);
+
+                        }
+                        else
+                        {
+                            GameplaySystem.combatManager.Inflict(characterStatusEffectReciever, i.Key.type);
+                        }
+                    }
+
+
+                }
+            }
+        }
+    }
+
 
 }
 
