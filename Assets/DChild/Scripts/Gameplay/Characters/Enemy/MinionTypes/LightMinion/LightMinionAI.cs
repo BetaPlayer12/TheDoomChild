47296d239de22f275153c8d9e57932c8be2fda12
TWICE Jihyan <giancarlo.toledo555@gmail.com>
2021-07-08 09:47:03 +0800

    [*] Scorchling AI Update

--- Assets/DChild/Scripts/Gameplay/Characters/Enemy/MinionTypes/LightMinion/LightMinionAI.cs
+++ Assets/DChild/Scripts/Gameplay/Characters/Enemy/MinionTypes/LightMinion/LightMinionAI.cs
@@ -432,6 +432,11 @@ namespace DChild.Gameplay.Characters.Enemies
 
         private IEnumerator StopAttackRoutine()
         {
+            m_trailFX.Stop();
+            for (int i = 0; i < m_fxSystem.Count; i++)
+            {
+                m_fxSystem[i].gameObject.SetActive(false);
+            }
             m_movement.Stop();
             m_animation.SetAnimation(0, m_info.attackEnd, false);
             yield return new WaitForAnimationComplete(m_animation.animationState, m_info.attackEnd);
@@ -557,6 +562,7 @@ namespace DChild.Gameplay.Characters.Enemies
                             else
                             {
                                 m_movement.Stop();
+                                m_stateHandle.Wait(State.Cooldown);
                                 StartCoroutine(StopAttackRoutine());
                                 //m_animation.SetAnimation(0, m_info.idleAnimation, true);
                             }
@@ -564,7 +570,7 @@ namespace DChild.Gameplay.Characters.Enemies
                     }
                     else
                     {
-                        m_turnState = State.ReevaluateSituation;
+                        m_turnState = State.Attacking;
                         m_stateHandle.SetState(State.Turning);
                     }
                     break;
@@ -588,7 +594,7 @@ namespace DChild.Gameplay.Characters.Enemies
                     }
                     else
                     {
-                        if (!m_wallSensor.allRaysDetecting)
+                        if (IsFacingTarget() && !m_wallSensor.allRaysDetecting)
                         {
                             m_currentCD = 0;
                             m_stateHandle.Wait(State.ReevaluateSituation);
