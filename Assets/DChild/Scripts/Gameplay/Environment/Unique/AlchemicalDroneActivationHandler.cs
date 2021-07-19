c3891f50dd03c00fecdd08933882d7d7695dcc4a
Adrianne Joseph Chua <chua.adrianne@gmail.com>
2021-07-19 15:27:51 +0800

    commit COD1 Dronescript

---
+++ Assets/DChild/Scripts/Gameplay/Environment/Unique/AlchemicalDroneActivationHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DChild.Gameplay.Environment
+{
+    public class AlchemicalDroneActivationHandler : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject[] m_alchemicaldrones;
+        public void activateBots()
+        {
+            for (int i = 0; i < m_alchemicaldrones.Length; i++)
+            {
+                m_alchemicaldrones[i].SetActive(true);
+            }
+        }
+        public void deactivateBots()
+        {
+            for (int i = 0; i < m_alchemicaldrones.Length; i++)
+            {
+                m_alchemicaldrones[i].SetActive(false);
+            }
+        }
+    }
+}
