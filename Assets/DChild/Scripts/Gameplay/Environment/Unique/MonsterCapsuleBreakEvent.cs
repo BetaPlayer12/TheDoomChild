1787baeb59f11836dc1370ac15c90bfc4b51cb09
Adrianne Joseph Chua <chua.adrianne@gmail.com>
2021-07-29 14:16:05 +0800

    commit lab scripts

--- Assets/DChild/Scripts/Gameplay/Environment/Unique/MonsterCapsuleBreakEvent.cs
+++ Assets/DChild/Scripts/Gameplay/Environment/Unique/MonsterCapsuleBreakEvent.cs
@@ -19,6 +19,8 @@ namespace DChild.Gameplay.Environment
         private GameObject m_brokenVersion;
         [SerializeField]
         private IAmbushingAI m_monster;
+        [SerializeField]
+        private Transform m_monsterLocation;
 
         private ICombatAIBrain m_brain;
 
@@ -26,7 +28,7 @@ namespace DChild.Gameplay.Environment
         {
             StopAllCoroutines();
             m_fixedVersion.SetActive(false);
-            m_monsterAnimation.gameObject.SetActive(false);
+            //m_monsterAnimation.gameObject.SetActive(false);
             m_glassBreakAnimation.gameObject.SetActive(false);
             m_brokenVersion.SetActive(true);
             m_monster.gameObject.SetActive(true);
@@ -36,16 +38,19 @@ namespace DChild.Gameplay.Environment
         public void ExecuteMonsterEscape()
         {
             StopAllCoroutines();
-            //StartCoroutine(MonsterEscapeRoutine());
+            StartCoroutine(MonsterEscapeRoutine());
             StartCoroutine(GlassBreakRoutine());
         }
 
         private IEnumerator MonsterEscapeRoutine()
         {
-            m_monsterAnimation.SetAnimation(0, "Escape", false);
-            yield return new WaitForAnimationComplete(m_monsterAnimation.animationState, "Escape");
-            m_monster.gameObject.SetActive(true);
+            //m_monsterAnimation.SetAnimation(0, "Escape", false);
+            //yield return new WaitForAnimationComplete(m_monsterAnimation.animationState, "Escape");
+            //m_monster.gameObject.SetActive(true);
             m_brain.SetTarget(GameplaySystem.playerManager.player.damageableModule);
+            Vector2 location = new Vector2(m_monsterLocation.transform.position.x, m_monsterLocation.transform.position.y);
+            m_monster.LaunchAmbush(location);
+            yield return null;
         }
 
         private IEnumerator GlassBreakRoutine()
@@ -55,6 +60,7 @@ namespace DChild.Gameplay.Environment
             m_glassBreakAnimation.SetAnimation(0, "Break", false);
             yield return new WaitForSeconds(4.5f);
             m_fixedVersion.SetActive(false);
+           
         }
 
         public void Reset()
@@ -62,11 +68,13 @@ namespace DChild.Gameplay.Environment
             StopAllCoroutines();
             m_fixedVersion.SetActive(true);
             m_nonAnimatedGlass.SetActive(true);
-            m_monsterAnimation.gameObject.SetActive(true);
+            //m_monsterAnimation.gameObject.SetActive(true);
             m_glassBreakAnimation.gameObject.SetActive(false);
             m_brokenVersion.SetActive(false);
             //m_monster.gameObject.SetActive(false);
             m_glassBreakAnimation.SetEmptyAnimation(0,0);
+            Vector2 location = new Vector2(m_monsterLocation.transform.position.x, m_monsterLocation.transform.position.y);
+            m_monster.PrepareAmbush(location);
         }
 
         private void Awake()
