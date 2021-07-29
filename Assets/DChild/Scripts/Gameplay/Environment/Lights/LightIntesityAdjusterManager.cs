d1cf6b2cfdff449c35af20f78728e77a2efd11ba
HolySoft_Ayan <betaplayer12@gmail.com>
2021-07-29 17:15:53 +0800

    [*] Light Adjusters now has common parent

---
+++ Assets/DChild/Scripts/Gameplay/Environment/Lights/LightIntesityAdjusterManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+#if UNITY_EDITOR
+#endif
+
+namespace DChild.Gameplay.Environment
+{
+    public class LightIntesityAdjusterManager : MonoBehaviour
+    {
+        [SerializeField]
+        private BaseLightIntensityAdjuster[] m_lightAdjusters;
+
+        public void SetIntensity(float value)
+        {
+            for (int i = 0; i < m_lightAdjusters.Length; i++)
+            {
+                m_lightAdjusters[i].SetIntensity(value);
+            }
+        }
+    }
+}
