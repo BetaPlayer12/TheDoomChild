69b513ac5112072182ef6958145c7e297f183a85
HolySoft_Ayan <betaplayer12@gmail.com>
2021-07-31 12:33:38 +0800

    [*] Laboratory Update for Capsule Monsters and Sliding Floor

--- Assets/DChild/Scripts/Gameplay/Environment/Unique/CapsuleMonsterVisualHandle.cs
+++ Assets/DChild/Scripts/Gameplay/Environment/Unique/CapsuleMonsterVisualHandle.cs
@@ -2,6 +2,9 @@
 using Spine.Unity;
 using System;
 using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 namespace DChild.Gameplay.Environment
 {
@@ -40,6 +43,10 @@ namespace DChild.Gameplay.Environment
         {
             var visualInfo = m_visualizationData.GetMonsterVisual(index);
             visualInfo.LoadVisualsTo(m_monsterVisual);
+#if UNITY_EDITOR
+            EditorUtility.SetDirty(m_monsterVisual);
+            EditorUtility.SetDirty(m_monsterVisual.transform);
+#endif
         }
 
         private void OnMonsterVisualDataChange()
