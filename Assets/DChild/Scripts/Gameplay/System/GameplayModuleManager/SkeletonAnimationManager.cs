ec87305bd5c9c03d11e883a0e18a206abc389c31
HolySoft_Ayan <betaplayer12@gmail.com>
2021-09-07 14:10:43 +0800

    [+] Gameplay Module System added for Optional Optimization

---
+++ Assets/DChild/Scripts/Gameplay/System/GameplayModuleManager/SkeletonAnimationManager.cs
@@ -0,0 +1,26 @@
+﻿using Spine.Unity;
+
+namespace DChild.Gameplay
+{
+    public class SkeletonAnimationManager : GameplayModuleManager<SpineAnimation>, IGameplayUpdateModule, IGameplayLateUpdateModule
+    {
+        public override string name => "SkeletonAnimationManager";
+
+        public void LateUpdateModule(float deltaTime)
+        {
+            for (int i = 0; i < m_list.Count; i++)
+            {
+                m_list[i].LateUpdateAnimation();
+            }
+
+        }
+
+        public void UpdateModule(float deltaTime)
+        {
+            for (int i = 0; i < m_list.Count; i++)
+            {
+                m_list[i].UpdateAnimation(deltaTime);
+            }
+        }
+    }
+}
