1a45f6437827e7087b9850150f22847375d1be0f
Adrianne Joseph Chua <chua.adrianne@gmail.com>
2021-09-10 12:06:47 +0800

    commit tracks,flameplatform

---
+++ Assets/DChild/Scripts/Gameplay/TimelineTracks/EntityStateTrack/CustomTrackAsset.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class CustomTrackAsset :  PlayableAsset
+{
+   
+    public override Playable CreatePlayable(PlayableGraph graph, GameObject owner)
+    {
+        var playable = ScriptPlayable<CustomTrackBehaviour>.Create(graph);
+
+        var CustomTrackBehaviour = playable.GetBehaviour();
+       
+
+        return playable;
+    }
+}
---
+++ Assets/DChild/Scripts/Gameplay/TimelineTracks/EntityStateTrack/CustomTrackBehaviour.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class CustomTrackBehaviour : PlayableBehaviour
+{
+    public override void ProcessFrame(Playable playable, FrameData info, object playerData)
+    {
+
+    }
+}
