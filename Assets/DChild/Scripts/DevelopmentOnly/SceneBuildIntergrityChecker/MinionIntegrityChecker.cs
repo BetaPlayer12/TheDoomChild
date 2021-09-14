9e822ec328590dca15527066f0e70f057282560b
HolySoft_Ayan <betaplayer12@gmail.com>
2021-09-14 15:48:20 +0800

    [+] Adding Build Integrity Report to warn Devs of "errors" that they wanted to know after the build (the ones that we can determine via code)

---
+++ Assets/DChild/Scripts/DevelopmentOnly/SceneBuildIntergrityChecker/MinionIntegrityChecker.cs
@@ -0,0 +1,31 @@
+﻿#if UNITY_EDITOR
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace DChildDebug
+{
+    public class MinionIntegrityChecker : IProcessSceneWithReport
+    {
+        public int callbackOrder => 1;
+
+        public void OnProcessScene(Scene scene, BuildReport report)
+        {
+            var enemies = GameObject.Find("Enemies");
+            if (enemies == null)
+            {
+                enemies = GameObject.Find("Enemy");
+            }
+
+            if (enemies != null)
+            {
+                if (enemies.activeInHierarchy == false)
+                {
+                    BuildIntegrityChecker.RecordReport(new BuildIntegrityReport(enemies.transform, enemies.gameObject, "Enemies Are Disabled"));
+                }
+            }
+        }
+    }
+}
+#endif
---
+++ Assets/DChild/Scripts/DevelopmentOnly/SceneBuildIntergrityChecker/SceneCameraIntergrityChecker.cs
@@ -0,0 +1,35 @@
+﻿//#if UNITY_EDITOR
+using Cinemachine;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace DChildDebug
+{
+    public class SceneCameraIntergrityChecker : IProcessSceneWithReport
+    {
+        public int callbackOrder => 3;
+
+        public static void ValidateIntegrity()
+        {
+            var cinemachineBrain = GameObject.FindObjectOfType<CinemachineBrain>();
+            if (cinemachineBrain != null && cinemachineBrain.enabled == false)
+            {
+                cinemachineBrain.enabled = true;
+            }
+
+            //var cinemachineCameras = GameObject.FindObjectsOfType<CinemachineVirtualCamera>();
+            //for (int i = 0; i < cinemachineCameras.Length; i++)
+            //{
+            //    cinemachineCameras[i].enabled = false;
+            //}
+        }
+
+        public void OnProcessScene(Scene scene, BuildReport report)
+        {
+            //ValidateIntegrity();
+        }
+    }
+}
+//#endif
