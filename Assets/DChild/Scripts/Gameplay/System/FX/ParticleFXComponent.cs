115a0108dbb3c2466c4a2f39ba9f8df480def912
HolySoft_Ayan <betaplayer12@gmail.com>
2021-09-15 17:42:18 +0800

    [*] World Map Location Button is now Sprite Sensitive [+] Particle FX Components added to have a more control to components in particle fx [!] Collider of a Door in Graveyard 3 is fixed [!] Explosive Barrel Explosion collider is fixed

---
+++ Assets/DChild/Scripts/Gameplay/System/FX/ParticleFXComponent.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace DChild.Gameplay
+{
+    public abstract class ParticleFXComponent : MonoBehaviour
+    {
+        public abstract void Initialize();
+        public abstract void SetActive(bool isActive);
+        public abstract void Stop();
+        public abstract void Reset();
+    }
+}
