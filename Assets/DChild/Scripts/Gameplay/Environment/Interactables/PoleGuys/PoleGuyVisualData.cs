15df8a32bccde331b29713d774665fab4b665a5e
HolySoft_Ayan <betaplayer12@gmail.com>
2021-11-15 13:59:05 +0800

    [+] Adding Pole Guys Prefab

---
+++ Assets/DChild/Scripts/Gameplay/Environment/Interactables/PoleGuys/PoleGuyVisualData.cs
@@ -0,0 +1,36 @@
+﻿using Spine.Unity;
+using UnityEngine;
+
+namespace DChild.Gameplay.Environment.Interractables
+{
+    [CreateAssetMenu(fileName = "MonsterCapsuleVisualData", menuName = "DChild/Gameplay/Environment/Pole Guy Visual Data")]
+    public class PoleGuyVisualData : ScriptableObject
+    {
+        [System.Serializable]
+        public class VisualInfo
+        {
+            [SerializeField]
+            private SkeletonDataAsset m_skeleton;
+            [SerializeField]
+            private Vector2 m_colliderOffset;
+            [SerializeField]
+            private Vector2 m_colliderSize;
+
+            public void LoadVisualsTo(SkeletonAnimation skeletonAnimation, BoxCollider2D collider)
+            {
+                skeletonAnimation.skeletonDataAsset = m_skeleton;
+               skeletonAnimation.Initialize(true);
+
+                collider.offset = m_colliderOffset;
+                collider.size = m_colliderSize;
+            }
+        }
+
+        [SerializeField]
+        private VisualInfo[] m_visualList;
+
+        public int count => m_visualList.Length;
+
+        public VisualInfo GetVisualInfo(int index) => m_visualList[index];
+    }
+}
