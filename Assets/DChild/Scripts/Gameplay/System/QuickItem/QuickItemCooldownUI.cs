9f711145318b83a0b8e4500292c99f527077c7d0
HolySoft_Ayan <betaplayer12@gmail.com>
2021-11-26 17:51:15 +0800

    [*] Improved QuickItemCooldownUI

--- Assets/DChild/Scripts/Gameplay/System/QuickItem/QuickItemCooldownUI.cs
+++ Assets/DChild/Scripts/Gameplay/System/QuickItem/QuickItemCooldownUI.cs
@@ -8,7 +8,9 @@ namespace DChild.Gameplay.Inventories.QuickItem
     public class QuickItemCooldownUI : MonoBehaviour
     {
         [SerializeField]
-        private Image m_cooldownIndicator;
+        private Image m_cooldownIndicatorShade;
+        [SerializeField]
+        private Image m_cooldownIndicatorFX;
         [SerializeField, MinValue(0.0001f)]
         private float m_transitionDuration = 0.75f;
         private bool m_showIndicator;
@@ -34,19 +36,38 @@ namespace DChild.Gameplay.Inventories.QuickItem
             var lerpSpeed = 1f / m_transitionDuration;
             var lerpValue = 0f;
             var visiblityValue = 0f;
-            var material = m_cooldownIndicator.materialForRendering;
+            var shadeMaterial = m_cooldownIndicatorShade.materialForRendering;
+            var fxMaterial = m_cooldownIndicatorFX.materialForRendering;
             do
             {
                 visiblityValue = Mathf.Lerp(from, to, lerpValue);
-                material.SetFloat("_VisibilityValue", visiblityValue);
+                SetMaterialValue(visiblityValue);
                 lerpValue += lerpSpeed * Time.fixedUnscaledDeltaTime;
                 yield return null;
             } while (lerpValue < 1);
 
             visiblityValue = Mathf.Lerp(from, to, 1f);
-            material.SetFloat("_VisibilityValue", visiblityValue);
+            SetMaterialValue(visiblityValue);
+
 
             yield return null;
+
+            void SetMaterialValue(float visibilityValue)
+            {
+                SetVisibility(shadeMaterial, visiblityValue);
+                SetVisibility(fxMaterial, visiblityValue);
+            }
+        }
+
+        private void SetVisibility(Material material, float visiblityValue)
+        {
+            material.SetFloat("_VisibilityValue", visiblityValue);
+        }
+
+        private void Awake()
+        {
+            SetVisibility(m_cooldownIndicatorShade.materialForRendering, 0);
+            SetVisibility(m_cooldownIndicatorFX.materialForRendering, 0);
         }
     }
 }
