8cf972faefacc5e53e6e5e6f80d9841f2711afc2
HolySoft_Ayan <betaplayer12@gmail.com>
2022-01-25 13:43:41 +0800

    [+] NavMap Components Upload

---
+++ Assets/DChild/Scripts/Gameplay/StoreMenu/NavigationMap/NavMapManager.cs
@@ -0,0 +1,24 @@
+﻿using DChild.Gameplay.Environment;
+using UnityEngine;
+
+namespace DChild.Gameplay.NavigationMap
+{
+    public class NavMapManager : MonoBehaviour
+    {
+        [SerializeField]
+        private NavMapInstantiator m_instantiator;
+        [SerializeField]
+        private NavMapTracker m_tracker;
+
+        public void UpdateConfiguration(Location location, Transform inGameReference, Vector2 mapReferencePoint, Vector2 calculationOffset)
+        {
+            if (m_instantiator.currentMap != location)
+            {
+                m_tracker.RemoveUIReferencesFromCurrentMap();
+                var map = m_instantiator.LoadMapFor(location);
+                m_tracker.SetReferencePointPosition(map, mapReferencePoint);
+            }
+            m_tracker.SetCalculationOffsets(calculationOffset);
+        }
+    }
+}
