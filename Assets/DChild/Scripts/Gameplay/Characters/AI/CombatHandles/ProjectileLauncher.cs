d3614536170c65f60829b0f6cc1e71bd34878706
HolySoft_Ayan <betaplayer12@gmail.com>
2022-01-28 16:12:08 +0800

    [+] Updated Breakables to use Addressables

--- Assets/DChild/Scripts/Gameplay/Characters/AI/CombatHandles/ProjectileLauncher.cs
+++ Assets/DChild/Scripts/Gameplay/Characters/AI/CombatHandles/ProjectileLauncher.cs
@@ -1,24 +1,9 @@
-﻿using DChild;
-using DChild.Gameplay.Pooling;
+﻿using DChild.Gameplay.Pooling;
 using DChild.Gameplay.Projectiles;
-using Sirenix.OdinInspector;
-using Spine.Unity;
 using UnityEngine;
 
 namespace DChild.Gameplay.Characters
 {
-    [System.Serializable]
-    public class ProjectileInfo
-    {
-        [SerializeField]
-        private GameObject m_projectile;
-        [SerializeField, MinValue(0.001f)]
-        private float m_speed;
-
-        public GameObject projectile => m_projectile;
-        public float speed => m_speed;
-    }
-
     [System.Serializable]
     public struct ProjectileLaunchHandle
     {
