f00e89d4c1bb3ad11f1fc44d4bea0d0beb50678f
DESKTOP-PU4PM46\USER <chua.adrianne@gmail.com>
2022-02-08 14:19:21 +0800

    commit Mappointofinteresttracker

---
+++ Assets/DChild/Scripts/Gameplay/StoreMenu/NavigationMap/MapPointOfInterestTrackerManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DChild.Gameplay.NavigationMap
+{
+    public class MapPointOfInterestTrackerManager : MonoBehaviour
+    {
+       [SerializeField]
+        private MapPointOfInterestTracker[] m_mapPointTracker;
+
+        //private void Track(object sender, EventActionArgs eventArgs)
+        //{
+            //UpdateMapPointTrackerInstanceState();
+        //}
+
+        // private void UpdateMapPointTrackerInstanceState()
+        //{
+
+
+
+       // } 
+    }
+}
