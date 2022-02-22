ce3f412e80d10a511b5b38f7190b869fe72e6de2
HolySoft_Ayan <betaplayer12@gmail.com>
2022-02-22 15:59:00 +0800

    [+] Breakable Crates Added

---
+++ Assets/Plugins/Pixel Crushers/Dialogue System/Scripts/Options/Timeline/Playables/PlaySequence/PlaySequenceShortcutBehaviour.cs
@@ -0,0 +1,29 @@
+﻿// Recompile at 01/07/2021 2:56:36 PM
+
+
+#if USE_TIMELINE
+#if UNITY_2017_1_OR_NEWER
+// Copyright (c) Pixel Crushers. All rights reserved.
+
+using System;
+using UnityEngine.Playables;
+
+namespace PixelCrushers.DialogueSystem
+{
+    [Serializable]
+    public class PlaySequenceShortcutBehaviour : PlayableBehaviour
+    {
+        public enum SequenceShortcut
+        {
+        }
+
+        public string m_reference;
+
+        public void Execute()
+        {
+
+        }
+    }
+}
+#endif
+#endif
