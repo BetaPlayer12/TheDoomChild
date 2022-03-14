5bba1741188f7f1e0d20004b4d8678960e560b4e
HolySoft_Ayan <betaplayer12@gmail.com>
2022-03-14 14:48:33 +0800

    [*] Dialogue knows when it is in a Timeline

--- Assets/DChild/Scripts/Gameplay/TimelineTracks/Dialogue/DialogueMixBehaviour.cs
+++ Assets/DChild/Scripts/Gameplay/TimelineTracks/Dialogue/DialogueMixBehaviour.cs
@@ -144,6 +144,7 @@ namespace PixelCrushers.DialogueSystem
             Message.AddListener<GameEventMessage>(OnContinueDiag);
             m_played.Clear();
             m_behaviours.Clear();
+            DChildStandardDialogueUI.isInCutscene = true;
         }
 
 
@@ -154,6 +155,7 @@ namespace PixelCrushers.DialogueSystem
             Message.RemoveListener<GameEventMessage>(OnContinueDiag);
             m_played.Clear();
             m_behaviours.Clear();
+            DChildStandardDialogueUI.isInCutscene = false;
         }
     }
 }
