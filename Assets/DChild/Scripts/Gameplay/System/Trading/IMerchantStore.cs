509faa7919ebd4de50d6ad01a1226327f39f92b6
HolySoft_Ayan <betaplayer12@gmail.com>
2022-03-30 15:49:38 +0800

    [+] Merchant Conditioned Wares Added

---
+++ Assets/DChild/Scripts/Gameplay/System/Trading/IMerchantStore.cs
@@ -0,0 +1,10 @@
+﻿using DChild.Gameplay.Inventories;
+
+namespace DChild.Menu.Trading
+{
+    public interface IMerchantStore
+    {
+        public void SetWaresReference(InventoryData inventory);
+        public void ResetWares();
+    }
+}
