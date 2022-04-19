4f796034bf547e08c118b97a16b92b22c19cdd4e
Adrianne Joseph Chua <chua.adrianne@gmail.com>
2022-04-19 14:09:17 +0800

    commit gammaadjuster

---
+++ Assets/DChild/Scripts/Settings/Visual/GammaAdjuster.cs
@@ -0,0 +1,40 @@
+﻿using DChild;
+using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.UI;
+
+namespace DChild.Configurations.Visuals
+{
+    public class GammaAdjuster : MonoBehaviour
+    {
+        public static GammaAdjuster instance;
+
+        [HideInInspector] public Volume renderingVolume;
+        LiftGammaGain liftGammaGain;
+
+
+
+        [Button]
+        public void SetGammaAlpha(float gammaAlpha)
+        {
+            liftGammaGain.gamma.Override(new Vector4(1f, 1f, 1f, gammaAlpha));
+        }
+
+        public float GetGammaAlpha() { return liftGammaGain.gamma.value.w; }
+        private void Awake()
+        {
+            instance = this;
+            renderingVolume = GetComponent<Volume>();
+            if (!renderingVolume.profile.TryGet(out liftGammaGain)) throw new System.NullReferenceException(nameof(liftGammaGain));
+        }
+
+        private void OnEnable()
+        {
+            SetGammaAlpha(GameSystem.settings.configuration.visualConfiguration.brightness);
+        }
+    }
+}
