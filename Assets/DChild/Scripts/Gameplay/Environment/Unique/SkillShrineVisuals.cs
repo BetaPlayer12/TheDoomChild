73ed03660b3e7e738f2dbc0413a31635f059e436
serpentdrake <krateusy@gmail.com>
2022-04-22 15:16:19 +0800

    [*]Modified PlayerSkill unloclerScript [+]Added Skill shrine visual handle and skillshrine visual script

---
+++ Assets/DChild/Scripts/Gameplay/Environment/Unique/SkillShrineVisuals.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+abstract public class SkillShrineVisuals : MonoBehaviour
+{
+    public void GetMaterial()
+    {
+
+    }
+
+    public void SetMaterial()
+    {
+
+    }
+}
