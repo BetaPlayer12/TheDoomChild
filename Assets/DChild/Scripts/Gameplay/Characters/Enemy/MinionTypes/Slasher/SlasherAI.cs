23446ebc4967c15fbaeac2d086c90ef0272fea1f
TWICE Jihyan <giancarlo.toledo555@gmail.com>
2022-04-25 10:37:20 +0800

    [*] City of the Dead Minions Box Crate detection fix

--- Assets/DChild/Scripts/Gameplay/Characters/Enemy/MinionTypes/Slasher/SlasherAI.cs
+++ Assets/DChild/Scripts/Gameplay/Characters/Enemy/MinionTypes/Slasher/SlasherAI.cs
@@ -165,7 +165,7 @@ namespace DChild.Gameplay.Characters.Enemies
                 //    && m_stateHandle.currentState != State.WaitBehaviourEnd)
                 //{
                 //}
-                if (!TargetBlocked() && !m_enablePatience)
+                if (/*!TargetBlocked() &&*/ !m_enablePatience)
                 {
                     m_selfCollider.enabled = false;
 
