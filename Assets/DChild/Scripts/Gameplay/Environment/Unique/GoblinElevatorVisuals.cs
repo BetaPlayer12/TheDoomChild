57e97a3accf0f38a9cef83277e08a3e0dbb72037
Adrianne Joseph Chua <chua.adrianne@gmail.com>
2022-05-18 14:39:22 +0800

    commit

--- Assets/DChild/Scripts/Gameplay/Environment/Unique/GoblinElevatorVisuals.cs
+++ Assets/DChild/Scripts/Gameplay/Environment/Unique/GoblinElevatorVisuals.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections;
+﻿using Sirenix.OdinInspector;
+using Spine.Unity;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,6 +8,21 @@ namespace DChild.Gameplay.Environment.VisualConfigurators
 {
     public class GoblinElevatorVisuals : MonoBehaviour
     {
-        
+        [System.Serializable]
+        public class DoorPanel
+        {
+            [SerializeField]
+            private SkeletonAnimation m_elevator;
+            [SerializeField, HorizontalGroup("Up"), ShowIf("m_elevator"), Spine.Unity.SpineAnimation(dataField = "m_elevator.skeletonDataAsset")]
+            private string m_upAnimation;
+            [SerializeField, HorizontalGroup("Down"), ShowIf("m_elevator"), Spine.Unity.SpineAnimation(dataField = "m_elevator.skeletonDataAsset")]
+            private string m_downAnimation;
+
+            public void SetAs(bool isGoingUp)
+            {
+                var chosenAnimation = isGoingUp ? m_upAnimation : m_downAnimation;
+                m_elevator.state.SetAnimation(0, chosenAnimation, false);
+            }
+        }
     }
 }
