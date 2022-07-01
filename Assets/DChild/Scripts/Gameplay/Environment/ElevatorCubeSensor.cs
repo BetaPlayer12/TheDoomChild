624f94ff72c08201f6aaeb1467618b7faf7408c6
Adrianne Joseph Chua <chua.adrianne@gmail.com>
2022-07-01 14:52:07 +0800

    commit LIB 1 and 2

---
+++ Assets/DChild/Scripts/Gameplay/Environment/ElevatorCubeSensor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace DChild.Gameplay.Environment
+{
+    public class ElevatorCubeSensor : MonoBehaviour
+    {
+        private Collider2D m_collider;
+        [SerializeField]
+        private UnityEvent m_event;
+       
+        void Start()
+        {
+            m_collider = GetComponent<Collider2D>();
+        }
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            var colliderGameObject = collision.gameObject;
+            if (colliderGameObject.GetComponent<CelestialCube>() != null)
+            {
+                m_event?.Invoke();
+            }
+        }
+
+
+        }
+}
