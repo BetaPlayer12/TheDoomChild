e65c47ecd6fe13ceb2889572f8368c95b145eeda
Juanito Jose Padilla <jjpadilla0915@gmail.com>
2022-08-24 11:40:42 +0800

    added Tome of Storm AI, changed Icycle Projectile in Tome of Frost

---
+++ Assets/DChild/Objects/Characters/Enemy/Minions/TomeOfElements/AttackObjects/StormCloud/StormCloud.cs
@@ -0,0 +1,28 @@
+﻿using DChild.Gameplay.Pooling;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DChild.Gameplay.Projectiles
+{
+    public class StormCloud : PoolableObject
+    {
+        public float timer;
+        // Start is called before the first frame update
+        void Start()
+        {
+            //CallPoolRequest();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            while (timer > 0)
+            {
+                timer -= GameplaySystem.time.deltaTime;
+            }
+            DestroyInstance();
+        }
+    }
+
+}
