85d660cea01da2d472c950848f297ec0ed8f502a
Juanito Jose Padilla <jjpadilla0915@gmail.com>
2022-08-31 16:28:46 +0800

    adjustment to poison blob and added tome of flame attacks

---
+++ Assets/DChild/Objects/Characters/Enemy/Minions/TomeOfElements/AttackObjects/DragonHeadSummon_Attack/FireDragonHead.cs
@@ -0,0 +1,64 @@
+﻿using DChild;
+using DChild.Gameplay.Characters;
+using Sirenix.OdinInspector;
+using Spine.Unity;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace DChild.Gameplay.Characters.Enemies
+{
+    public class FireDragonHead : MonoBehaviour
+    {
+        [SerializeField]
+        private SpineRootAnimation m_spine;
+
+        public GameObject flameEffect;
+        public GameObject flameCollider;
+
+        public void InitializeField(SpineRootAnimation spineRoot)
+        {
+            m_spine = spineRoot;
+        }
+
+        [SerializeField, Spine.Unity.SpineAnimation(dataField = "m_skeletonFAnimation")]
+        private string m_attackAnimation;
+
+        public void PlayAttackAnimation()
+        {
+            Debug.Log("Attack Animation Played");
+            StartCoroutine(AttackRoutine());
+        }
+
+        public IEnumerator AttackRoutine()
+        {
+            Debug.Log("Attack Routine run");
+            m_spine.SetAnimation(0, m_attackAnimation, false);
+            Debug.Log("animation state: " + m_attackAnimation);
+            yield return new WaitForAnimationComplete(m_spine.animationState, m_attackAnimation);
+            gameObject.SetActive(false);
+            yield return null;
+        }
+
+        public void ShootFire()
+        {
+            Debug.Log("Fire shot");
+            flameEffect.SetActive(true);
+            flameCollider.SetActive(true);
+        }
+
+        public void OffFire()
+        {
+            Debug.Log("Fire turned off");
+            flameEffect.SetActive(false);
+            flameCollider.SetActive(false);
+        }
+
+        private void Start()
+        {
+            
+        }
+    }
+}
