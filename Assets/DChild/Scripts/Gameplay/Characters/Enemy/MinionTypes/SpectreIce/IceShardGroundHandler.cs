6e6de8459b3a07fa1b5ea71230b360be72b93f2f
Adrianne Joseph Chua <chua.adrianne@gmail.com>
2022-09-22 13:49:19 +0800

    commit spectreice

---
+++ Assets/DChild/Scripts/Gameplay/Characters/Enemy/MinionTypes/SpectreIce/IceShardGroundHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace DChild.Gameplay.Environment
+{
+    public class IceShardGroundHandler : MonoBehaviour
+    {
+        public RaySensor m_sensor;
+        [SerializeField]
+        private UnityEvent m_grounddetected;
+        private bool m_platform = false;
+        
+
+
+        void Update()
+        {
+            if (m_platform == false)
+            {
+                m_sensor.Cast();
+                RaycastHit2D[] RayGameObject = m_sensor.GetValidHits();
+                for (int i = 0; i < RayGameObject.Length; i++)
+                {
+                    if (RayGameObject[i].collider.gameObject.layer == LayerMask.NameToLayer("Environment"))
+                    {
+                        m_platform = true;
+                        m_grounddetected?.Invoke();
+                    }
+
+                }
+            }
+
+        }
+    }
+}
