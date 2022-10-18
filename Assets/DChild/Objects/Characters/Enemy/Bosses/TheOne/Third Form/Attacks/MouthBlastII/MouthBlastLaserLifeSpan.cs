d19a5bbb76088db5d5220b64525d751ba9c4a030
Juanito Jose Padilla <jjpadilla0915@gmail.com>
2022-10-18 12:11:13 +0800

    Mouth Blast II attack for The One III

---
+++ Assets/DChild/Objects/Characters/Enemy/Bosses/TheOne/Third Form/Attacks/MouthBlastII/MouthBlastLaserLifeSpan.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DChild.Gameplay.Pooling;
+
+namespace DChild.Gameplay.Projectiles
+{
+    public class MouthBlastLaserLifeSpan : PoolableObject
+    {
+        [SerializeField]
+        private float m_timer;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            m_timer -= GameplaySystem.time.deltaTime;
+
+            if (m_timer < 0)
+            {
+                DestroyInstance();
+            }
+        }
+    }
+}
+
