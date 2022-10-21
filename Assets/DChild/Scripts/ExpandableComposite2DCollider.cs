135118c1aeef832239f378dfc66c92ac7e045de7
HolySoft_Ayan <betaplayer12@gmail.com>
2022-10-21 14:45:41 +0800

    [+] Expandable Composite 2D Collider

---
+++ Assets/DChild/Scripts/ExpandableComposite2DCollider.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace DChild
+{
+    public class ExpandableComposite2DCollider : MonoBehaviour
+    {
+        [SerializeField]
+        private CompositeCollider2D m_compositeCollider;
+        [SerializeField]
+        private Collider2D[] m_colliderExpansions;
+
+        public void ActivateExpansions()
+        {
+            for (int i = 0; i < m_colliderExpansions.Length; i++)
+            {
+                m_colliderExpansions[i].enabled = true;
+            }
+            m_compositeCollider.GenerateGeometry();
+        }
+
+        public void DeactivateExpansions()
+        {
+            for (int i = 0; i < m_colliderExpansions.Length; i++)
+            {
+                m_colliderExpansions[i].enabled = false;
+            }
+            m_compositeCollider.GenerateGeometry();
+        }
+    }
+
+}
