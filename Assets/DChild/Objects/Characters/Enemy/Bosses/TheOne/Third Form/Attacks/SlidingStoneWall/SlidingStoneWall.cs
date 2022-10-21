338275104ca3cde02b6ce8263397b8f33caf671e
Juanito Jose Padilla <jjpadilla0915@gmail.com>
2022-10-21 15:14:34 +0800

    Progress on The One III

---
+++ Assets/DChild/Objects/Characters/Enemy/Bosses/TheOne/Third Form/Attacks/SlidingStoneWall/SlidingStoneWall.cs
@@ -0,0 +1,69 @@
+﻿using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DChild.Gameplay.Pooling;
+
+namespace DChild.Gameplay.Characters.Enemies
+{
+    public class SlidingStoneWall : PoolableObject
+    {
+        private bool m_wallIsGrounded;
+        private bool m_slideWall;
+
+        [SerializeField, TabGroup("Sensors")]
+        private RaySensor m_wallSensor;
+        [SerializeField, TabGroup("Sensors")]
+        private RaySensor m_floorSensor;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            m_wallIsGrounded = false;
+            m_slideWall = false;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (!m_wallIsGrounded)
+            {
+                StartCoroutine(WallSmashOnGround());
+            }
+
+            if (m_slideWall)
+            {
+                StartCoroutine(SlideWall());
+            }
+        }
+
+        private IEnumerator WallSmashOnGround()
+        {
+            if (!m_floorSensor.isDetecting)
+            {
+                transform.Translate(Vector3.down);
+            }
+
+            yield return new WaitForSeconds(2f);
+
+            m_wallIsGrounded = true;
+            m_slideWall = true;
+        }
+
+        private IEnumerator SlideWall()
+        {
+            if (!m_wallSensor.isDetecting)
+            {
+                transform.Translate(Vector3.right);
+            }
+            else
+            {
+                //Destroy Wall animation here
+                DestroyInstance();
+            }
+
+            yield return new WaitForSeconds(1f);
+        }
+    }
+}
+
