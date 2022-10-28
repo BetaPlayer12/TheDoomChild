0446d16014c4f6ea0b0d397b6ad742f36a61b1d5
Juanito Jose Padilla <jjpadilla0915@gmail.com>
2022-10-28 21:00:19 +0800

    Fixed Tentacle Ground Stab behavior The One III

--- Assets/DChild/Objects/Characters/Enemy/Bosses/TheOne/Third Form/Attacks/GroundStabTentacle/TentacleLifeSpan.cs
+++ Assets/DChild/Objects/Characters/Enemy/Bosses/TheOne/Third Form/Attacks/GroundStabTentacle/TentacleLifeSpan.cs
@@ -2,16 +2,71 @@
 using System.Collections.Generic;
 using UnityEngine;
 using DChild.Gameplay.Pooling;
+using Sirenix.OdinInspector;
+using DChild.Gameplay.Characters;
+using Spine.Unity;
 
 namespace DChild.Gameplay.Projectiles
 {
     public class TentacleLifeSpan : PoolableObject
     {
         public float m_timer;
+
+        private BoxCollider2D m_tentacleHitBox;
+
+        [SerializeField]
+        private GameObject[] safeZones;
+
+        [SerializeField, TabGroup("Reference")]
+        protected SpineRootAnimation m_animation;
+        [SerializeField]
+        private SkeletonAnimation m_skeletonAnimation;
+        [SerializeField, Spine.Unity.SpineAnimation(dataField = "m_skeletonAnimation")]
+        private string m_anticipationLoopAnimation;
+        [SerializeField, Spine.Unity.SpineAnimation(dataField = "m_skeletonAnimation")]
+        private string m_anticipationStartAnimation;
+        [SerializeField, Spine.Unity.SpineAnimation(dataField = "m_skeletonAnimation")]
+        private string m_attackAnimation;
+        [SerializeField, Spine.Unity.SpineAnimation(dataField = "m_skeletonAnimation")]
+        private string m_retractAnimation;
+        [SerializeField, Spine.Unity.SpineAnimation(dataField = "m_skeletonAnimation")]
+        private string m_stayAnimation;
+
+        public IEnumerator StabRoutine()
+        {
+            m_animation.SetAnimation(0, m_anticipationStartAnimation, false);
+            yield return new WaitForAnimationComplete(m_animation.animationState, m_anticipationStartAnimation);
+
+            m_animation.SetAnimation(0, m_attackAnimation, false);
+            yield return new WaitForAnimationComplete(m_animation.animationState, m_attackAnimation);
+
+            StartCoroutine(TentacleStay());
+        }
+
+        public IEnumerator TentacleStay()
+        {
+            m_animation.SetAnimation(0, m_stayAnimation, false);
+            yield return new WaitForAnimationComplete(m_animation.animationState, m_stayAnimation);
+        }
+
+        public IEnumerator Retract()
+        {
+            foreach (GameObject safeZone in safeZones)
+            {
+                safeZone.SetActive(false);
+            }
+            m_animation.SetAnimation(0, m_retractAnimation, false);
+            yield return new WaitForAnimationComplete(m_animation.animationState, m_retractAnimation);
+            
+            DestroyInstance();
+        }
+
         // Start is called before the first frame update
         void Start()
         {
-
+            m_tentacleHitBox = this.GetComponent<BoxCollider2D>();
+            m_tentacleHitBox.enabled = false;
+            StartCoroutine(StabRoutine());
         }
 
         // Update is called once per frame
@@ -21,9 +76,10 @@ namespace DChild.Gameplay.Projectiles
 
             if (m_timer < 0)
             {
-                DestroyInstance();
+                StartCoroutine(Retract());
             }
         }
+
     }
 }
 
