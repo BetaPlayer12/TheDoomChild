930b025edf8172dc24893a281220061795ffbde4
HolySoft_Ayan <betaplayer12@gmail.com>
2022-11-24 17:06:10 +0800

    [-]Cleaned Up Meta head Files [*] Scene Loading Accomodates Both SceneManager and Addressable Load

--- Assets/DChild/Scripts/SceneLoader.cs
+++ Assets/DChild/Scripts/SceneLoader.cs
@@ -32,14 +32,14 @@ namespace DChild
         public void SetAsActiveZone(string sceneName) => m_activeZone = sceneName;
 #endif
 
-        public void LoadZone(string sceneName, bool withLoadingScene, Action ToCallAfterSceneDone)
+        public void LoadZone(SceneInfo scene, bool withLoadingScene, Action ToCallAfterSceneDone)
         {
             CallAfterSceneDone = ToCallAfterSceneDone;
             LoadingHandle.LoadingDone += AfterSceneDone;
-            LoadZone(sceneName, withLoadingScene);
+            LoadZone(scene, withLoadingScene);
         }
 
-        public void LoadZone(string sceneName, bool withLoadingScene)
+        public void LoadZone(SceneInfo scene, bool withLoadingScene)
         {
             if (withLoadingScene)
             {
@@ -52,19 +52,19 @@ namespace DChild
 
                 if (m_gameplaySceneActive == false)
                 {
-                    LoadingHandle.LoadScenes(m_gameplayScene.sceneName);
+                    LoadingHandle.LoadScenes(m_gameplayScene);
                     m_gameplaySceneActive = true;
                 }
                 //if (m_activeZone != sceneName)
                 //{
-                LoadingHandle.LoadScenes(sceneName);
+                LoadingHandle.LoadScenes(scene);
                 //}
-                SceneManager.LoadScene(m_loadingScene.sceneName, LoadSceneMode.Additive);
+                GameSystem.sceneManager.LoadSceneAsync(m_loadingScene.sceneName);
             }
             else
             {
                 RoomActivityManager.UnloadAllRooms();
-                if (m_activeZone != string.Empty && m_activeZone != sceneName)
+                if (m_activeZone != string.Empty && m_activeZone != scene.sceneName)
                 {
                     LoadingHandle.UnloadScenes(m_activeZone);
                     m_activeZone = string.Empty;
@@ -73,9 +73,9 @@ namespace DChild
                 {
                     SceneManager.LoadSceneAsync(m_gameplayScene.sceneName, LoadSceneMode.Additive);
                 }
-                SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+                SceneManager.LoadSceneAsync(scene.sceneName, LoadSceneMode.Additive);
             }
-            m_activeZone = sceneName;
+            m_activeZone = scene.sceneName;
         }
 
         public void LoadMainMenu()
@@ -85,11 +85,11 @@ namespace DChild
                 LoadingHandle.UnloadScenes(m_activeZone);
                 m_activeZone = string.Empty;
             }
-            LoadingHandle.UnloadScenes(m_gameplayScene.sceneName);
+            LoadingHandle.UnloadScenes(m_gameplayScene);
             m_gameplaySceneActive = false;
-            LoadingHandle.LoadScenes(m_mainMenu.sceneName);
+            LoadingHandle.LoadScenes(m_mainMenu);
             Time.timeScale = 1;
-            Addressables.LoadSceneAsync(m_loadingScene.sceneName, LoadSceneMode.Additive);
+            GameSystem.sceneManager.LoadSceneAsync(m_loadingScene.sceneName);
         }
 
         private void AfterSceneDone(object sender, EventActionArgs eventArgs)
