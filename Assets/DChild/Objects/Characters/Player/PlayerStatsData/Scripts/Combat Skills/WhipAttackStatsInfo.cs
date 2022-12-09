62306b9acdbc58ede600b3e1adf0942c17f46f6b
TWICE Jihyan <giancarlo.toledo555@gmail.com>
2022-12-09 15:30:37 +0800

    [*] Player Character Configurator Update

--- Assets/DChild/Objects/Characters/Player/PlayerStatsData/Scripts/Combat Skills/WhipAttackStatsInfo.cs
+++ Assets/DChild/Objects/Characters/Player/PlayerStatsData/Scripts/Combat Skills/WhipAttackStatsInfo.cs
@@ -8,9 +8,17 @@ public struct WhipAttackStatsInfo
     [SerializeField]
     private float m_aerialGravity;
     public float aerialGravity => m_aerialGravity;
+    [SerializeField]
+    private float m_whipMovementCooldown;
+    public float whipMovementCooldown => m_whipMovementCooldown;
+    [SerializeField]
+    private Vector2 m_momentumVelocity;
+    public Vector2 momentumVelocity => m_momentumVelocity;
 
     public void CopyInfo(WhipAttackStatsInfo reference)
     {
         m_aerialGravity = reference.aerialGravity;
+        m_whipMovementCooldown = reference.whipMovementCooldown;
+        m_momentumVelocity = reference.momentumVelocity;
     }
 }
