ee44244b94bd51e52006042e00e95c6820b172b8
HolySoft_Ayan <betaplayer12@gmail.com>
2023-02-24 11:57:35 +0800

    [*]Updating Army Battle To Have Attack Group

--- Assets/DChild/Scripts/Gameplay/System/ArmyBattle/ArmyAttackGroupData.cs
+++ Assets/DChild/Scripts/Gameplay/System/ArmyBattle/ArmyAttackGroupData.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
 
 namespace DChild.Gameplay.ArmyBattle
 {
@@ -8,34 +9,26 @@ namespace DChild.Gameplay.ArmyBattle
         [SerializeField]
         private string m_groupName;
         [SerializeField]
+        private UnitType m_unitType;
+#if UNITY_EDITOR
+        [InfoBox("@\"Total Power: \" + GetTotalPower()", InfoMessageType = InfoMessageType.None)]
+#endif
+        [SerializeField, InlineEditor(Expanded = true)]
         private ArmyCharacter[] m_members;
 
+        public string groupName => m_groupName;
         public int memberCount => m_members.Length;
-        public ArmyCharacter GetMember(int index) => m_members[index];
-    }
-
-    [System.Serializable]
-    public class ArmyAttackGroup
-    {
-        private ArmyCharacter[] m_members;
-        private bool m_isAvailable;
-
-        public ArmyAttackGroup(ArmyAttackGroupData data)
-        {
-
-        }
-
-        public string groupName;
-        public int totalPower;
-
-        public int membersCount => m_members.Length;
-        public bool isAvailable => m_isAvailable;
-
+        public UnitType unitType => m_unitType;
         public ArmyCharacter GetMember(int index) => m_members[index];
 
-        public void SetAvailability(bool isAvailable)
+        private int GetTotalPower()
         {
-            m_isAvailable = isAvailable;
+            var power = 0;
+            for (int i = 0; i < m_members.Length; i++)
+            {
+                power += m_members[i].power;
+            }
+            return power;
         }
     }
 }
---
+++ Assets/DChild/Scripts/Gameplay/System/ArmyBattle/ArmyAttackGroupUI.cs
@@ -0,0 +1,27 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace DChild.Gameplay.ArmyBattle.UI
+{
+    public class ArmyAttackGroupUI : MonoBehaviour
+    {
+        [SerializeField]
+        private TextMeshProUGUI m_groupLabel;
+        [SerializeField]
+        private TextMeshProUGUI m_powerLabel;
+
+        public void Display(ArmyAttackGroup group)
+        {
+            UpdateText(m_groupLabel, group.groupName);
+            UpdateText(m_powerLabel, group.GetTotalPower().ToString());
+        }
+
+        private void UpdateText(TextMeshProUGUI label, string value)
+        {
+            if (label != null)
+            {
+                label.text = value;
+            }
+        }
+    }
+}
