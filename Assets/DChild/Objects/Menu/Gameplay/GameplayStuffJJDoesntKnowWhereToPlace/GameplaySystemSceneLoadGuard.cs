7de40ddb4fb8a588ff03bf0f893eb669961fa38e
HolySoft_Ayan <betaplayer12@gmail.com>
2023-03-08 14:47:53 +0800

    [+] Adding Soul Skill Debug

--- Assets/DChild/Objects/Menu/Gameplay/GameplayStuffJJDoesntKnowWhereToPlace/GameplaySystemSceneLoadGuard.cs
+++ Assets/DChild/Objects/Menu/Gameplay/GameplayStuffJJDoesntKnowWhereToPlace/GameplaySystemSceneLoadGuard.cs
@@ -1,4 +1,5 @@
 using DChild.Menu;
+using Doozy.Runtime.Nody;
 using Doozy.Runtime.Signals;
 using Holysoft.Event;
 using System;
@@ -10,8 +11,27 @@ public class GameplaySystemSceneLoadGuard : MonoBehaviour
 {
     [SerializeField]
     private SignalSender m_UIInitializeSignal;
+    [SerializeField]
+    private FlowController m_uiFlow;
 
-    void Awake()
+    private bool m_hasFlowStarted = false;
+
+    private IEnumerator InitializeUIRoutine()
+    {
+        m_uiFlow.onStart.AddListener(SetFlowHasStarted);
+
+        while (m_hasFlowStarted ==false)
+        {
+            yield return null;
+        }
+        m_uiFlow.onStart.RemoveListener(SetFlowHasStarted);
+        yield return new WaitForSecondsRealtime(1);
+        m_UIInitializeSignal.SendSignal();
+
+        void SetFlowHasStarted() => m_hasFlowStarted = true;
+    }
+
+    private void Awake()
     {
         if (LoadingHandle.isLoading)
         {
@@ -21,7 +41,7 @@ public class GameplaySystemSceneLoadGuard : MonoBehaviour
         else
         {
             Debug.LogError("No Wait for Loading to Be done");
-            m_UIInitializeSignal.SendSignal();
+            StartCoroutine(InitializeUIRoutine());
         }
     }
 
