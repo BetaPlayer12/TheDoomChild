46494d9be74dabf4084117836655cda5247fde13
HolySoft_Ayan <betaplayer12@gmail.com>
2023-03-09 14:14:44 +0800

    [+] Adding NavMap Instance

---
+++ Assets/DChild/Scripts/Gameplay/StoreMenu/NavigationMap/NavigationMapInstance.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace DChild.Gameplay.NavigationMap
+{
+    public class NavigationMapInstance : MonoBehaviour
+    {
+        [SerializeField]
+        private NavMapFogOfWarUI[] m_sceneFogOFWars;
+
+        public NavMapFogOfWarUI GetFogOfWarOfScene(int index) => m_sceneFogOFWars[index];
+    }
+}
