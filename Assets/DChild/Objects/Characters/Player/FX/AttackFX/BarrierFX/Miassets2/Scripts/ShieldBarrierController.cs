9d6b4a681f8cef2b43b633e9070c929c87a35405
TWICE Jihyan <giancarlo.toledo555@gmail.com>
2023-03-16 15:40:20 +0800

    [*] Barrier FX Update

--- Assets/DChild/Objects/Characters/Player/FX/AttackFX/BarrierFX/Miassets2/Scripts/ShieldBarrierController.cs
+++ Assets/DChild/Objects/Characters/Player/FX/AttackFX/BarrierFX/Miassets2/Scripts/ShieldBarrierController.cs
@@ -1,3 +1,6 @@
+using System;
+using DChild;
+using DChild.Gameplay.Combat;
 using UnityEngine;
 
 public class ShieldBarrierController : MonoBehaviour
@@ -7,6 +10,8 @@ public class ShieldBarrierController : MonoBehaviour
     [SerializeField] private float destroyDelay = 2f;
     [SerializeField] private string enemyTag = "EnemyProjectile";
     [SerializeField] private int currentLife;
+    [SerializeField]
+    private Damageable m_damageable;
 
     public Animator animator;
     public string damagedTriggerName = "Damaged";
@@ -15,25 +20,26 @@ public class ShieldBarrierController : MonoBehaviour
     private void Start()
     {
         currentLife = maxLife;
+        m_damageable.DamageTaken += DamageTaken;
+        m_damageable.DamageBlock += DamageTaken;
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
+    private void DamageTaken(object sender, Damageable.DamageEventArgs eventArgs)
     {
-        if (other.CompareTag(enemyTag))
-        {
-            TakeDamage();
-        }
+        Debug.Log("Barrier taken Damage");
+        TakeDamage();
     }
 
     private void TakeDamage()
     {
+        Debug.Log("Barrier taken Damage FUNCTION");
         currentLife -= damageAmount;
         animator.SetTrigger(damagedTriggerName);
 
         if (currentLife <= 0)
         {
-            animator.SetTrigger(destroyedTriggerName);
-            Destroy(gameObject, destroyDelay);
+            //animator.SetTrigger(destroyedTriggerName);
+            //Destroy(gameObject, destroyDelay);
         }
     }
 }
