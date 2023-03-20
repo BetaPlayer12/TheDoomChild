b21810907f7798579fe43950384e5702808a9859
TWICE Jihyan <giancarlo.toledo555@gmail.com>
2023-03-20 16:37:57 +0800

    [*] teleport skull update

--- Assets/DChild/Scripts/Gameplay/Characters/Player/BattleAbilities/TeleportingSkull.cs
+++ Assets/DChild/Scripts/Gameplay/Characters/Player/BattleAbilities/TeleportingSkull.cs
@@ -40,7 +40,7 @@ namespace DChild.Gameplay.Characters.Players.BattleAbilityModule
         public void TeleportToProjectile()
         {
             m_canTeleport = false;
-            m_character.transform.position = m_spawnedProjectile.transform.position;
+            m_character.transform.position = Mathf.Abs(RoofPosition(m_spawnedProjectile.transform.position).y - m_spawnedProjectile.transform.position.y) < 5f ? new Vector3(m_spawnedProjectile.transform.position.x, m_spawnedProjectile.transform.position.y - m_character.height) : m_spawnedProjectile.transform.position;
             m_spawnedProjectile.CallPoolRequest();
             base.AttackOver();
         }
@@ -48,6 +48,58 @@ namespace DChild.Gameplay.Characters.Players.BattleAbilityModule
         public void Execute()
         {
             m_canTeleport = true;
+        }
+
+        protected Vector2 RoofPosition(Vector2 startPoint)
+        {
+            int hitCount = 0;
+            //RaycastHit2D hit = Physics2D.Raycast(m_projectilePoint.position, Vector2.down,  1000, DChildUtility.GetEnvironmentMask());
+            RaycastHit2D[] hit = Cast(startPoint, Vector2.up, 100f, true, out hitCount, true);
+            if (hit != null)
+            {
+                Debug.DrawRay(startPoint, hit[0].point);
+                return hit[0].point;
+            }
+            return Vector2.zero;
+            //var hitPos = (new Vector2(m_projectilePoint.position.x, Vector2.down.y) * hit[0].distance);
+            //return hitPos;
+        }
+
+        private static ContactFilter2D m_contactFilter;
+        private static RaycastHit2D[] m_hitResults;
+        private static bool m_isInitialized;
+
+        private static void Initialize()
+        {
+            if (m_isInitialized == false)
+            {
+                m_contactFilter.useLayerMask = true;
+                m_contactFilter.SetLayerMask(DChildUtility.GetEnvironmentMask());
+                //m_contactFilter.SetLayerMask(Physics2D.GetLayerCollisionMask(DChildUtility.GetEnvironmentMask()));
+                m_hitResults = new RaycastHit2D[16];
+                m_isInitialized = true;
+            }
+        }
+
+        protected static RaycastHit2D[] Cast(Vector2 origin, Vector2 direction, float distance, bool ignoreTriggers, out int hitCount, bool debugMode = false)
+        {
+            Initialize();
+            m_contactFilter.useTriggers = !ignoreTriggers;
+            hitCount = Physics2D.Raycast(origin, direction, m_contactFilter, m_hitResults, distance);
+#if UNITY_EDITOR
+            if (debugMode)
+            {
+                if (hitCount > 0)
+                {
+                    Debug.DrawRay(origin, direction * m_hitResults[0].distance, Color.cyan, 1f);
+                }
+                else
+                {
+                    Debug.DrawRay(origin, direction * distance, Color.cyan, 1f);
+                }
+            }
+#endif
+            return hitCount == 0 ? null : m_hitResults;
         }
     }
 }
