874bde5a5af6e75d3a8adbb8a41ada007418563b
HolySoft_Ayan <betaplayer12@gmail.com>
2023-05-29 15:14:27 +0800

    [+] Adding Notification Handling

---
+++ Assets/DChild/Scripts/Gameplay/UI/NotificationHandling/IUINotificationManager.cs
@@ -0,0 +1,28 @@
+﻿using DChild.Gameplay.Characters.Players;
+using DChild.Gameplay.Characters.Players.SoulSkills;
+using DChild.Gameplay.Items;
+using DChild.Gameplay.Systems;
+using DChild.Gameplay.Systems.Lore;
+using PixelCrushers.DialogueSystem;
+
+namespace DChild.Gameplay.UI
+{
+    public interface IUINotificationManager
+    {
+        void ShowJournalUpdateNotification();
+
+        void QueueNotification(PrimarySkill skill);
+
+        void QueueNotification(SoulSkill soulSkill);
+
+        void QueueNotification(LoreData data);
+
+        void QueueNotification(ItemData itemData);
+
+        void QueueNotification(QuestEntryArgs questInfo);
+
+        void QueueNotification(LootList lootList);
+
+        void QueueNotification(StoreNotificationType notificationType);
+    }
+}
