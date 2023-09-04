bfce0edfbc879fdcbb7fcb339983d93e0ad5acc4
Juanito Jose Padilla <jjpadilla0915@gmail.com>
2023-09-04 15:06:03 +0800

    Forced book pick up on New Game Intro

--- Assets/DChild/Scripts/Gameplay/System/PlayerIntroFunctions.cs
+++ Assets/DChild/Scripts/Gameplay/System/PlayerIntroFunctions.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using DChild.Gameplay.Combat;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace DChild.Gameplay.Characters.Players.Modules
 {
@@ -8,6 +10,10 @@ namespace DChild.Gameplay.Characters.Players.Modules
     {
         [SerializeField]
         private List<IntroActions> allowedActions;
+        [SerializeField]
+        private UnityEvent m_forceInteract;
+
+        private (Damageable damageable, Character character) m_targetTuple;
 
         public void EnableIntroControls()
         {
@@ -22,6 +28,19 @@ namespace DChild.Gameplay.Characters.Players.Modules
         public void EnabledAllowedAction()
         {
             GameplaySystem.playerManager.EnableIntroAction(allowedActions);
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.tag != "Sensor")
+            {
+                var target = collision.GetComponentInParent<ITarget>();
+                if (target.CompareTag(Character.objectTag))
+                {
+                    m_targetTuple = (collision.GetComponentInParent<Damageable>(), collision.GetComponentInParent<Character>());
+                    m_forceInteract.Invoke();
+                }
+            }
         }
     }
 }
