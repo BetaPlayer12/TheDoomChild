8ae9184f8e38d0b7082e98e3df2cda52b13c4d40
HolySoft_Ayan <betaplayer12@gmail.com>
2023-09-13 18:57:10 +0800

    #3788 Refactorization Attempt

---
+++ Assets/DChild/Scripts/Gameplay/StoreMenu/Codex/BestiaryCodex/BestiaryCodexIndexButton.cs
@@ -0,0 +1,12 @@
+using DChild.Menu.Bestiary;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace DChild.Menu.Codex.Bestiary
+{
+    public class BestiaryCodexIndexButton : CodexIndexButton<BestiaryData>
+    {
+    }
+
+
+}
---
+++ Assets/DChild/Scripts/Gameplay/StoreMenu/Codex/CodexHandle.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DChild.Menu.Codex
+{
+    public class CodexHandle : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
