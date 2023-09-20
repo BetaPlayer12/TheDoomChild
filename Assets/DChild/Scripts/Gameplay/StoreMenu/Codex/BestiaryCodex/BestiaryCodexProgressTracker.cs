43becf3fccfb95e903bb0eb3572ff1f419ee8369
HolySoft_Ayan <betaplayer12@gmail.com>
2023-09-20 14:07:26 +0800

    #3788 Refactorization Of Bestiary to Codex Index Added Tracking of Bestiary Data from Kills back

--- Assets/DChild/Scripts/Gameplay/StoreMenu/Codex/BestiaryCodex/BestiaryCodexProgressTracker.cs
+++ Assets/DChild/Scripts/Gameplay/StoreMenu/Codex/BestiaryCodex/BestiaryCodexProgressTracker.cs
@@ -1,9 +1,48 @@
-﻿using DChild.Menu.Bestiary;
+﻿using DChild.Gameplay;
+using DChild.Gameplay.Combat;
+using DChild.Menu.Bestiary;
+using UnityEngine;
+using DChild.Gameplay.UI;
 
 namespace DChild.Menu.Codex.Bestiary
 {
     public class BestiaryCodexProgressTracker : CodexProgressTracker<BestiaryList, BestiaryData>
     {
+        [SerializeField]
+        private Attacker m_attacker;
 
+        public void RecordCreatureToBestiary(int ID)
+        {
+            if (HasInfoOf(ID) == false)
+            {
+                GameplaySystem.gamplayUIHandle.notificationManager.QueueNotification(StoreNotificationType.Bestiary);
+            }
+            SetProgress(ID, true);
+        }
+
+        public void RecordCreatureToBestiary(BestiaryData data)
+        {
+            RecordCreatureToBestiary(data.id);
+        }
+
+        private void Awake()
+        {
+            m_attacker.TargetDamaged += OnTargetDamaged;
+        }
+
+        private void OnTargetDamaged(object sender, CombatConclusionEventArgs eventArgs)
+        {
+            if (eventArgs.target.instance.isAlive == false && eventArgs.target.hasBestiaryData)
+            {
+                RecordCreatureToBestiary(eventArgs.target.bestiaryID);
+            }
+        }
+
+#if UNITY_EDITOR
+        public void Initialize(GameObject character)
+        {
+            m_attacker = character.GetComponent<Attacker>();
+        }
+#endif
     }
 }
