d710ba6d57b5f29319d498c00f11ee2b3c18caa9
serpentdrake <krateusy@gmail.com>
2023-09-22 13:58:36 +0800

    [+] Implemented Character Codex UI and Functionality

---
+++ Assets/DChild/Scripts/Gameplay/StoreMenu/Codex/CharacterCodex/CharacterCodexHandle.cs
@@ -0,0 +1,14 @@
+using DChild.Codex.Characters;
+using DChild.Menu.Codex;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DChild.Codex.Characters
+{
+    public class CharacterCodexHandle : CodexHandle<CharacterCodexData, CharacterCodexData>
+    {
+
+    }
+
+}
