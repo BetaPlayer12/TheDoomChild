92bf325a569c8f3c32277586f7d44d98f31568e2
serpentdrake <krateusy@gmail.com>
2023-09-26 11:25:03 +0800

    [+] Implemented lore codex tab navigation

---
+++ Assets/DChild/Scripts/Gameplay/StoreMenu/Lore/LoreCodexList.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DChild.Codex.Lore
+{
+    [CreateAssetMenu(fileName = "LoreCodexList", menuName = "DChild/Database/Lore Codex List")]
+    public class LoreCodexList : DatabaseAssetList<LoreCodexData>
+    {
+
+
+    }
+}
+
