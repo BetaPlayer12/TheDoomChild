e5ed2dbf80be24f9489b4945501bf3149f5bb53b
Adrianne Joseph Chua <chua.adrianne@gmail.com>
2023-10-20 14:24:40 +0800

    commit weaponconversion

---
+++ Assets/DChild/Scripts/Gameplay/System/Inventory/WeaponShardConversionHandler.cs
@@ -0,0 +1,25 @@
+using DChild.Gameplay;
+using DChild.Gameplay.Characters.Players;
+using DChild.Gameplay.Inventories;
+using DChild.Gameplay.Items;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponShardConversionHandler : MonoBehaviour
+{
+    [SerializeField]
+    private ItemData m_item;
+    [SerializeField]
+    private IShardCompletionHandle m_completionHandle;
+
+    protected static Player m_player;
+    private static UsableItemData m_pickUpItem;
+
+    private void ItemConversionReward()
+    {
+        m_player.inventory.RemoveItem(m_item);
+        m_completionHandle.Execute(m_player);
+    }
+   
+}
