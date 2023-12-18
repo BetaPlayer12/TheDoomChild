422d94ee7c567c4f2ce0aa50fb60baa0555699da
HolySoft_Ayan <betaplayer12@gmail.com>
2023-12-18 12:24:34 +0800

    Revert "[*] Setting Up Part 2"
    This reverts commit 0c1aa9f3924d1b86a81a90280976cb72d82893f7.

--- Assets/Debug/TEST_TRACKERS/PRE_ALPHA/PlayerActivityTracker.cs
+++ Assets/Debug/TEST_TRACKERS/PRE_ALPHA/PlayerActivityTracker.cs
@@ -1,45 +1,18 @@
-using DChild.Gameplay;
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace DChild.Testing.PreAlpha
+public class PlayerActivityTracker : MonoBehaviour
 {
-    public static class PreAlphaUtility
-    {
-        public const string COMPONENTMENU_ADDRESS = "Testing/PreAlpha/";
-    }
-
-    [AddComponentMenu(PreAlphaUtility.COMPONENTMENU_ADDRESS+ "PlayerActivityTracker")]
-    public class PlayerActivityTracker : MonoBehaviour
+    // Start is called before the first frame update
+    void Start()
     {
         
     }
 
-    public class PreAlphaSerializer : MonoBehaviour
+    // Update is called once per frame
+    void Update()
     {
-        private void OnPostDeserialization(object sender, CampaignSlotUpdateEventArgs eventArgs)
-        {
-            throw new NotImplementedException();
-        }
-
-        private void OnPreSerialization(object sender, CampaignSlotUpdateEventArgs eventArgs)
-        {
-            throw new NotImplementedException();
-        }
-
-        private void Start()
-        {
-            GameplaySystem.campaignSerializer.PreSerialization += OnPreSerialization;
-            GameplaySystem.campaignSerializer.PostDeserialization += OnPostDeserialization;
-        }
+        
     }
-
-    [System.Serializable]
-    public class PlayerActivityData
-    {
-
-    }
-
 }
