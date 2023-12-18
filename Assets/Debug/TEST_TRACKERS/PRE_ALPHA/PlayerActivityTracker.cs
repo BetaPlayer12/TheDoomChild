8c2288e75e9d3f064084984c4530f90e53475c53
HolySoft_Ayan <betaplayer12@gmail.com>
2023-12-18 15:02:30 +0800

    Setting Up PreAlpha Trackers

---
+++ Assets/Debug/TEST_TRACKERS/PRE_ALPHA/PlayerActivityTracker.cs
@@ -0,0 +1,71 @@
+using DChild.Gameplay;
+using Sirenix.Serialization;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+namespace DChild.Testing.PreAlpha
+{
+
+    [AddComponentMenu(PreAlphaUtility.COMPONENTMENU_ADDRESS + "PlayerActivityTracker")]
+    public class PlayerActivityTracker : MonoBehaviour
+    {
+
+    }
+
+    public class PlayerDeathCountTracker: MonoBehaviour
+    {
+        private void Start()
+        {
+            
+        }
+    }
+
+    public class PreAlphaSerializer : MonoBehaviour
+    {
+        private void OnPostDeserialization(object sender, CampaignSlotUpdateEventArgs eventArgs)
+        {
+            Save();
+        }
+
+        private void OnPreSerialization(object sender, CampaignSlotUpdateEventArgs eventArgs)
+        {
+        }
+
+        private void Save()
+        {
+            var id = GameplaySystem.campaignSerializer.slot.id;
+            var filePath = $"{Application.persistentDataPath}/PRa{id}.txt";
+            PreAlphaData data = null;
+            byte[] bytes = SerializationUtility.SerializeValue(data, DataFormat.JSON);
+            File.WriteAllBytes(filePath, bytes);
+            Debug.Log("PreAlpha Logged " +
+                       $"\n {filePath}");
+        }
+
+        private void Load(PreAlphaData data)
+        {
+
+        }
+
+        private void Start()
+        {
+            GameplaySystem.campaignSerializer.PreSerialization += OnPreSerialization;
+            GameplaySystem.campaignSerializer.PostDeserialization += OnPostDeserialization;
+        }
+
+        private void OnDestroy()
+        {
+            Save();
+        }
+    }
+
+    [System.Serializable]
+    public class PreAlphaData
+    {
+
+    }
+
+}
