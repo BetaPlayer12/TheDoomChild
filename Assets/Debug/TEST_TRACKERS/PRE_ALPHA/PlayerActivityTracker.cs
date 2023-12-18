0c1aa9f3924d1b86a81a90280976cb72d82893f7
HolySoft_Ayan <betaplayer12@gmail.com>
2023-12-18 12:19:49 +0800

    [*] Setting Up Part 2

--- Assets/Debug/TEST_TRACKERS/PRE_ALPHA/PlayerActivityTracker.cs
+++ Assets/Debug/TEST_TRACKERS/PRE_ALPHA/PlayerActivityTracker.cs
@@ -1,18 +1,45 @@
+using DChild.Gameplay;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerActivityTracker : MonoBehaviour
+namespace DChild.Testing.PreAlpha
 {
-    // Start is called before the first frame update
-    void Start()
+    public static class PreAlphaUtility
+    {
+        public const string COMPONENTMENU_ADDRESS = "Testing/PreAlpha/";
+    }
+
+    [AddComponentMenu(PreAlphaUtility.COMPONENTMENU_ADDRESS+ "PlayerActivityTracker")]
+    public class PlayerActivityTracker : MonoBehaviour
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public class PreAlphaSerializer : MonoBehaviour
     {
-        
+        private void OnPostDeserialization(object sender, CampaignSlotUpdateEventArgs eventArgs)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnPreSerialization(object sender, CampaignSlotUpdateEventArgs eventArgs)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Start()
+        {
+            GameplaySystem.campaignSerializer.PreSerialization += OnPreSerialization;
+            GameplaySystem.campaignSerializer.PostDeserialization += OnPostDeserialization;
+        }
     }
+
+    [System.Serializable]
+    public class PlayerActivityData
+    {
+
+    }
+
 }
