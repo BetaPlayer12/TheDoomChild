0761fa09b44b4c11859b03a0f6e660719249f97a
Neowb <stephenjohngavaran22@gmail.com>
2024-03-13 14:06:46 +0800

    [*] modified the petal rain of mother mantis [*] modified the petal stalagmite timing of the growth and wilt [*] modified the mother mantis bulb destroy [*] modified the instantiation of the mother mantis petal seed rain [+] reimported the spine animation of the mother mantis

--- Assets/DChild/Scripts/Gameplay/Characters/Enemy/BossTypes/MotherMantis/MotherMantisSeed.cs
+++ Assets/DChild/Scripts/Gameplay/Characters/Enemy/BossTypes/MotherMantis/MotherMantisSeed.cs
@@ -19,6 +19,8 @@ public class MotherMantisSeed : MonoBehaviour
     [SerializeField]
     private MotherMantisAI m_motherMantisAI;
 
+    public EventAction<EventActionArgs> OnStalagmiteSummoned;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (DChildUtility.IsAnEnvironmentLayerObject(collision.gameObject))
@@ -31,14 +33,17 @@ public class MotherMantisSeed : MonoBehaviour
     {
         Instantiate(m_bladeOfGrass, transform.position, Quaternion.identity);
         yield return new WaitForSeconds(1f);
-        //var stalagmite = Instantiate(m_thornVine, m_position, Quaternion.identity);
-        //stalagmite.GetComponent<PetalStalagtite>().Growing += OnPetalRain;
         Instantiate(m_thornVine, m_position, Quaternion.identity);
         yield return null;
     }
-    private void OnPetalRain(object sender, EventActionArgs eventArgs)
+    /*private void OnPetalRain(object sender, EventActionArgs eventArgs)
     {
         m_motherMantisAI.OnPetalRain += OnPetalRain;
         //throw new NotImplementedException();
-    }
+    }*/
+    /*private void OnMantisLand(object sender, EventActionArgs eventArgs)
+    {
+        m_motherMantisAI.OnMantisLand += OnMantisLand;
+        //throw new NotImplementedException();
+    }*/
 }
