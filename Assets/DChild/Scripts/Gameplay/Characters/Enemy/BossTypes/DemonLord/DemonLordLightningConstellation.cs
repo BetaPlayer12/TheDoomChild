94095e33681e865e35f46f8102db598307e645dd
serpentdrake <krateusy@gmail.com>
2024-03-27 18:06:16 +0800

    [*]Created Contellation Collider Setup

---
+++ Assets/DChild/Scripts/Gameplay/Characters/Enemy/BossTypes/DemonLord/DemonLordLightningConstellation.cs
@@ -0,0 +1,70 @@
+﻿using Holysoft.Event;
+using UnityEngine;
+using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace DChild.Gameplay.Characters.Enemies
+{
+    public class DemonLordLightningConstellation : SerializedMonoBehaviour
+    {
+        public enum Type
+        {
+
+            Leo,
+            Aries,
+            Taurus,
+            Cancer,
+            Scorpio,
+            _COUNT
+        }
+
+        [SerializeField]
+        private Animator m_animator;
+        [SerializeField]
+        private Dictionary<Type, string> m_typeToAnimatorParameterPair;
+
+        private LightningConstellationColliderConfigurator[] m_lightningConfigurators;
+
+        public event EventAction<EventActionArgs> SpellEnd;
+
+        public void ExecuteSpell(Type type)
+        {
+            this.gameObject.SetActive(true);
+            StopAllCoroutines();
+            StartCoroutine(SpellRoutine(type));
+        }
+
+        private IEnumerator SpellRoutine(Type type)
+        {
+            m_animator.SetTrigger(m_typeToAnimatorParameterPair[type]);
+
+            ////Wait for Animator to actually do the Animation;
+            //yield return new WaitForSeconds(1f);
+
+            //for (int i = 0; i < m_lightningConfigurators.Length; i++)
+            //{
+            //    m_lightningConfigurators[i].ReorientCollider();
+            //}
+
+            var currentAnimation = m_animator.GetCurrentAnimatorStateInfo(0);
+            while (currentAnimation.normalizedTime < 1)
+                yield return null;
+
+
+            SpellEnd?.Invoke(this, EventActionArgs.Empty);
+            this.gameObject.SetActive(false);
+        }
+
+        private void Awake()
+        {
+            m_lightningConfigurators = GetComponentsInChildren<LightningConstellationColliderConfigurator>();
+        }
+
+        private void Start()
+        {
+            //Deactivate at start so that line configurator will not keep updating
+            this.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/DChild/Scripts/Gameplay/Characters/Enemy/BossTypes/DemonLord/LightningConstellationColliderConfigurator.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace DChild.Gameplay.Characters.Enemies
+{
+    public class LightningConstellationColliderConfigurator : MonoBehaviour
+    {
+        [SerializeField]
+        private LineRenderer m_reference;
+        [SerializeField]
+        private EdgeCollider2D m_collider;
+        [SerializeField]
+        private float m_activeColliderThreshold;
+
+        private MaterialPropertyBlock m_propertyBlock;
+        private int m_shaderAlphaPropertyID;
+        private List<Vector2> m_colliderPoints;
+
+        public void ReorientCollider()
+        {
+            m_colliderPoints.Clear();
+            for (int i = 0; i < m_reference.positionCount; i++)
+            {
+                m_colliderPoints.Add(m_reference.GetPosition(i));
+            }
+            m_collider.SetPoints(m_colliderPoints);
+            m_collider.offset = m_reference.transform.position * -1;
+        }
+
+        private void SyncColliderActivationWithMaterial()
+        {
+            m_reference.GetPropertyBlock(m_propertyBlock);
+            var enableCollider = m_propertyBlock.GetFloat(m_shaderAlphaPropertyID) >= m_activeColliderThreshold;
+            m_collider.enabled = enableCollider;
+
+        }
+
+        private void Awake()
+        {
+            m_colliderPoints = new List<Vector2>();
+            m_propertyBlock = new MaterialPropertyBlock();
+            m_shaderAlphaPropertyID = Shader.PropertyToID("_Alpha");
+        }
+
+        private void Update()
+        {
+            SyncColliderActivationWithMaterial();
+            ReorientCollider();
+        }
+
+    }
+}
