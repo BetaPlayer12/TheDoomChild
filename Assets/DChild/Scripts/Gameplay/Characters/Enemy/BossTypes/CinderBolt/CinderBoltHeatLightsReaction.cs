25fe06d87d312b200ebf2235204f9e20f8505654
HolySoft_Ayan <betaplayer12@gmail.com>
2024-04-02 15:08:53 +0800

    Adding Heat Visuals to Cinderblot behaviour

---
+++ Assets/DChild/Scripts/Gameplay/Characters/Enemy/BossTypes/CinderBolt/CinderBoltHeatLightsReaction.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Spine.Unity;
+using UnityEngine;
+
+namespace DChild.Gameplay.Characters.Enemies
+{
+    public class CinderBoltHeatLightsReaction : CinderBoltHeatReaction
+    {
+        [System.Serializable]
+        private class Configuration
+        {
+            [SerializeField]
+            private int m_heatThreshold;
+            [SerializeField, SpineSlot]
+            private string m_slot;
+            [SerializeField, SpineAttachment]
+            private string m_attachment;
+
+            public int heatThreshold => m_heatThreshold;
+            public string slot => m_slot;
+            public string attachment => m_attachment;
+        }
+
+        [SerializeField]
+        private Configuration[] m_configurations;
+
+        public override void HandleReaction(SpineAnimation spineAnimation, int heatValue)
+        {
+            for (int i = 0; i < m_configurations.Length; i++)
+            {
+                var config = m_configurations[i];
+                if (heatValue >= config.heatThreshold)
+                {
+                    spineAnimation.skeletonAnimation.skeleton.SetAttachment(config.slot, config.attachment);
+                }
+                else
+                {
+                    spineAnimation.skeletonAnimation.skeleton.SetAttachment(config.slot, null);
+                }
+            }
+        }
+    }
+}
