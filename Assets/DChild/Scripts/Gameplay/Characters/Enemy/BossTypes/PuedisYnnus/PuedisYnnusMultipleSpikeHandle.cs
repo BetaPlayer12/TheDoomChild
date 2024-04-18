e779f51abf41403bc959eecae0496181abab18f8
HolySoft_Ayan <betaplayer12@gmail.com>
2024-04-18 14:24:11 +0800

    #5276 Integrating Multiple Spike Visuals

--- Assets/DChild/Scripts/Gameplay/Characters/Enemy/BossTypes/PuedisYnnus/PuedisYnnusMultipleSpikeHandle.cs
+++ Assets/DChild/Scripts/Gameplay/Characters/Enemy/BossTypes/PuedisYnnus/PuedisYnnusMultipleSpikeHandle.cs
@@ -4,6 +4,15 @@ namespace DChild.Gameplay.Characters.Enemies
 {
     public class PuedisYnnusMultipleSpikeHandle : MonoBehaviour
     {
+        [SerializeField]
+        private Transform m_spikes;
+        [SerializeField]
+        private Vector2 m_spikesHiddenPosition;
+        [SerializeField]
+        private Vector2 m_spikesrevealed;
+        [SerializeField]
+        private ParticleSystem m_dustFX;
+
         public void Grow()
         {
             gameObject.SetActive(true);
