2aa692562deeab89b61bac9e629a6c19835aa6d5
Juanito Jose Padilla <jjpadilla0915@gmail.com>
2024-04-19 14:52:48 +0800

    [+] Created King Pus Damageable script

---
+++ Assets/DChild/Scripts/Gameplay/Characters/Enemy/BossTypes/KingPus/KingPusDamageable.cs
@@ -0,0 +1,65 @@
+using DChild.Gameplay.Characters.Enemies;
+using Holysoft.Event;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DChild.Gameplay.Combat
+{
+    public class KingPusDamageable : Damageable
+    {
+        [SerializeField]
+        private KingPusAI m_kingPusAI;
+        [SerializeField]
+        private Phase m_currentPhase = Phase.PhaseOne;
+
+        public event EventAction<EventActionArgs> PhaseChangeTime;
+
+        enum Phase
+        {
+            PhaseOne,
+            PhaseTwo, 
+            PhaseThree,
+        }
+
+        private void Start()
+        {
+            PhaseChangeTime += OnChangePhase;
+            m_currentPhase = Phase.PhaseOne;
+        }
+
+        private void OnChangePhase(object sender, EventActionArgs eventArgs)
+        {
+            m_currentPhase++;
+            RestoreHPForPhaseChange();
+        }
+
+        private void RestoreHPForPhaseChange()
+        {
+            Heal(1000);
+        }
+
+        public override void TakeDamage(int totalDamage, DamageType type)
+        {
+            if(totalDamage <= base.health.currentValue)
+            {
+                base.TakeDamage(totalDamage, type);
+            }
+            else if(totalDamage >= base.health.currentValue) 
+            {
+                if (m_currentPhase == Phase.PhaseThree)
+                {
+                    Debug.Log("Going to Die!");
+                    base.TakeDamage(totalDamage, type);
+                }
+                else
+                {
+                    Debug.Log("Phase Change Time!");
+                    PhaseChangeTime?.Invoke(this, new EventActionArgs());
+                }
+            }
+        }
+    }
+}
+
