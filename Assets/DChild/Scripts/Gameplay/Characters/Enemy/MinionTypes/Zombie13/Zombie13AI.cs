79e696e60fef6b5152396505358ce6c9476d49dc
MSI\Work <betaplayer12@gmail.com>
2024-06-24 13:55:37 +0800

    commit spiked runner

--- Assets/DChild/Scripts/Gameplay/Characters/Enemy/MinionTypes/Zombie13/Zombie13AI.cs
+++ Assets/DChild/Scripts/Gameplay/Characters/Enemy/MinionTypes/Zombie13/Zombie13AI.cs
@@ -25,7 +25,9 @@ namespace DChild.Gameplay.Characters.Enemies
             [SerializeField, TitleGroup("Movement")]
             private MovementInfo m_walk = new MovementInfo();
             public MovementInfo walk => m_walk;
-
+            [SerializeField, TitleGroup("Movement")]
+            private MovementInfo m_hop = new MovementInfo();
+            public MovementInfo hop => m_hop;
             //Attack Behaviours
             [SerializeField, TitleGroup("Combat")]
             private SimpleAttackInfo m_attack = new SimpleAttackInfo();
@@ -79,6 +81,7 @@ namespace DChild.Gameplay.Characters.Enemies
             {
 #if UNITY_EDITOR
                 m_walk.SetData(m_skeletonDataAsset);
+                m_hop.SetData(m_skeletonDataAsset);
                 m_attack.SetData(m_skeletonDataAsset);
 
                 m_idle1Animation.SetData(m_skeletonDataAsset);
@@ -554,7 +557,7 @@ namespace DChild.Gameplay.Characters.Enemies
                         {
                             m_turnState = State.Patrol;
                             m_animation.EnableRootMotion(true, false);
-                            m_animation.SetAnimation(0, m_info.attack.animation, true);
+                            m_animation.SetAnimation(0, m_info.hop.animation, true);
                             var characterInfo = new PatrolHandle.CharacterInfo(m_character.centerMass.position, m_character.facing);
                         }
                         else
