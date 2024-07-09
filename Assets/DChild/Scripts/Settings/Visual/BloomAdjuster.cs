b1634959da612a2f9887c1c49e95d28ea5dfc028
HolySoft_Ayan <betaplayer12@gmail.com>
2024-07-09 16:53:00 +0800

    [*] Refactory Post Processing For Zones [+] Added Bloom to Post Process Adjustments

--- Assets/DChild/Scripts/Settings/Visual/BloomAdjuster.cs
+++ Assets/DChild/Scripts/Settings/Visual/BloomAdjuster.cs
@@ -5,32 +5,32 @@ using UnityEngine.Rendering.Universal;
 namespace DChild.Configurations.Visuals
 {
     [System.Serializable]
-    public class GammaAdjuster : IPostProcessAdjusterModule
+    public class BloomAdjuster : IPostProcessAdjusterModule
     {
         [SerializeField]
-        private float m_gamma;
-        private LiftGammaGain m_liftGammaGain = null;
+        private bool m_enabled = true;
+        private Bloom m_bloom = null;
 
         public void ApplyConfiguration(PostProcessConfiguration configuration)
         {
-            if (m_liftGammaGain != null)
+            if (m_bloom != null)
             {
-                m_liftGammaGain.gamma.Override(new Vector4(1f, 1f, 1f, configuration.gamma));
-                m_gamma = configuration.gamma;
+                m_bloom.active = configuration.isBloomEnabled;
+                m_enabled = configuration.isBloomEnabled;
             }
         }
 
         public void ModifyConfiguration(ref PostProcessConfiguration configuration)
         {
-            configuration.gamma = m_gamma;
+            configuration.isBloomEnabled = m_enabled;
         }
 
         public bool ValidatePostProcess(Volume volume, ref string message)
         {
-            var result = volume.profile.TryGet(out LiftGammaGain liftGammaGain);
+            var result = volume.profile.TryGet(out Bloom bloom);
             if (result)
             {
-                m_liftGammaGain = liftGammaGain;
+                m_bloom = bloom;
             }
             else
             {
