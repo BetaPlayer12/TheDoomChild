689e46b6aa2acdcc69e6aa29041cd5bf02d9d0ff
HolySoft_Ayan <betaplayer12@gmail.com>
2024-07-18 14:08:02 +0800

    [*]# 5778 Royal Death Guard Death Stench Wave Projectile Has Animated Collision

--- Assets/DChild/Scripts/Gameplay/Characters/Enemy/BossTypes/RoyalDeathGuard/RoyalDeathGuardDeathStenchWave.cs
+++ Assets/DChild/Scripts/Gameplay/Characters/Enemy/BossTypes/RoyalDeathGuard/RoyalDeathGuardDeathStenchWave.cs
@@ -16,28 +16,33 @@ namespace DChild.Gameplay.Characters.Enemies
         private SimpleAttackProjectile m_leftProjectile;
 
         private ParticleSystem m_rightProjectileFX;
+        private Animator m_rightProjectileAnim;
         private ParticleSystem m_leftProjectileFX;
+        private Animator m_leftProjectileAnim;
 
-        [Button,HideInEditorMode]
+        [Button, HideInEditorMode]
         public void Execute()
         {
-            LaunchProjectile(m_rightProjectile, m_rightProjectileFX, Vector2.right);
-            LaunchProjectile(m_leftProjectile, m_leftProjectileFX, Vector2.left);
+            LaunchProjectile(m_rightProjectile, m_rightProjectileFX, m_rightProjectileAnim, Vector2.right);
+            LaunchProjectile(m_leftProjectile, m_leftProjectileFX, m_leftProjectileAnim, Vector2.left);
             m_leftProjectile.transform.localScale = Vector3.one; //Force Left Projectile to Be at the right orientation cuz Projectile.Launch changes its orientation
         }
 
-        private void LaunchProjectile(SimpleAttackProjectile projectile, ParticleSystem fx, Vector2 direction)
+        private void LaunchProjectile(SimpleAttackProjectile projectile, ParticleSystem fx, Animator animator, Vector2 direction)
         {
             projectile.gameObject.SetActive(true);
             projectile.transform.localPosition = direction * m_launchXOffset;
             projectile.Launch(direction, m_speed);
             fx.Play();
+            animator.SetTrigger("Launch");
         }
 
         private void Awake()
         {
             m_rightProjectileFX = m_rightProjectile.GetComponent<ParticleSystem>();
+            m_rightProjectileAnim = m_rightProjectile.GetComponent<Animator>();
             m_leftProjectileFX = m_leftProjectile.GetComponent<ParticleSystem>();
+            m_leftProjectileAnim = m_leftProjectile.GetComponent<Animator>();
         }
     }
 }
