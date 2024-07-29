14f6d459b1fee6e00f2444531d485752533fb41a
HolySoft_Ayan <betaplayer12@gmail.com>
2024-07-29 15:11:13 +0800

    [+] Guided Light Handle Added

---
+++ Assets/DChild/Scripts/Gameplay/Environment/Unique/GuidedLightHandle.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace DChild.Gameplay.Environment.Interractables
+{
+    public class GuidedLightHandle : MonoBehaviour
+    {
+        [SerializeField]
+        private Renderer m_renderer;
+        [SerializeField]
+        private Transform m_tracker;
+
+        private MaterialPropertyBlock m_materialPropertyBlock;
+        private int m_trackerPositionParamID;
+
+        private void Awake()
+        {
+            m_materialPropertyBlock = new MaterialPropertyBlock();
+            m_trackerPositionParamID = Shader.PropertyToID("_LightGuide_Position");
+        }
+
+        private void LateUpdate()
+        {
+            m_renderer.GetPropertyBlock(m_materialPropertyBlock);
+            m_materialPropertyBlock.SetVector(m_trackerPositionParamID, m_tracker.position);
+            m_renderer.SetPropertyBlock(m_materialPropertyBlock);
+        }
+    }
+
+}
