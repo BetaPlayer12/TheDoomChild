19869219b732d7b1b70a29d43eeadfe8d232e51b
HolySoft_Ayan <betaplayer12@gmail.com>
2024-07-29 15:00:36 +0800

    [+] Snared Status Effect Partial Add

---
+++ Assets/DChild/Scripts/Gameplay/Characters/Player/PrimarySkill/PlayerBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace DChild.Gameplay.Characters.Players
+{
+    [Flags]
+    public enum PlayerBehaviour
+    {
+        Jump = 1<< 0,
+        Attack = 1<< 1,
+        Movement =  1<< 2,
+
+        [HideInInspector]
+        All = Jump | Attack | Movement,
+        [HideInInspector]
+        None = 0
+    }
+}
---
+++ Assets/DChild/Scripts/Gameplay/System/Combat/StatusEffect/StatusModules/PlayerBehaviourLock.cs
@@ -0,0 +1,38 @@
+﻿using DChild.Gameplay.Characters.Players;
+using UnityEngine;
+
+namespace DChild.Gameplay.Combat.StatusAilment
+{
+    [System.Serializable]
+    public class PlayerBehaviourLock : IStatusEffectModule
+    {
+        [SerializeField]
+        private PlayerBehaviour[] m_behaviourToLock;
+
+        public IStatusEffectModule GetInstance() => this;
+
+        public void Start(Character character)
+        {
+            if (character == GameplaySystem.playerManager.player.character)
+            {
+                var behaviourModule = GameplaySystem.playerManager.player.behaviourModule;
+                for (int i = 0; i < m_behaviourToLock.Length; i++)
+                {
+                    behaviourModule.SetModuleActive(m_behaviourToLock[i], false);
+                }
+            }
+        }
+
+        public void Stop(Character character)
+        {
+            if (character == GameplaySystem.playerManager.player.character)
+            {
+                var behaviourModule = GameplaySystem.playerManager.player.behaviourModule;
+                for (int i = 0; i < m_behaviourToLock.Length; i++)
+                {
+                    behaviourModule.SetModuleActive(m_behaviourToLock[i], true);
+                }
+            }
+        }
+    }
+}
