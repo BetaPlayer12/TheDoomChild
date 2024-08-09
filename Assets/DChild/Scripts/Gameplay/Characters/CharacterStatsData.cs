24886978e1bf4597b986136d5a5c708efb9ebd3a
Juanito Jose Padilla <jjpadilla0915@gmail.com>
2024-08-09 12:12:57 +0800

    [*] Undid change to CSD script since it was redundant and functionality needed was present in Attacker script

--- Assets/DChild/Scripts/Gameplay/Characters/CharacterStatsData.cs
+++ Assets/DChild/Scripts/Gameplay/Characters/CharacterStatsData.cs
@@ -24,10 +24,5 @@ namespace DChild.Gameplay
         public AttackResistanceData attackResistance => m_attackResistance;
         public StatusEffectChanceData statusInfliction => m_statusInfliction;
         public StatusEffectChanceData statusResistanceData => m_statusResistanceData;
-
-        public void SetAttackData(AttackData damage)
-        {
-            m_damage = damage;
-        }
     }
 }
