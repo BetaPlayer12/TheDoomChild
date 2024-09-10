dbea95c9e3905b5b0b3ccfd743fa954e3e936080
Juanito Jose Padilla <jjpadilla0915@gmail.com>
2024-09-10 15:01:36 +0800

    [+] Created ISpecialSkillModule script

---
+++ Assets/DChild/Scripts/Gameplay/System/ArmyBattle/ISpecialSkillModule.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DChild.Gameplay.ArmyBattle
+{
+    public interface ISpecialSkillModule
+    {
+ 
+    }
+}
+
