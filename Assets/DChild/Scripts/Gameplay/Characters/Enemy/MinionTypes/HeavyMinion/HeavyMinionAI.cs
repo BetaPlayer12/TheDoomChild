36176c6f46bbda0e6c557e264f4d5e0ebaecbccd
Tommi Pallarco <tommipallarco@gmail.com>
2024-09-10 13:48:34 +0800

    [*] enabled FireGolem body collision damage after attack and adjusted attack data damage

--- Assets/DChild/Scripts/Gameplay/Characters/Enemy/MinionTypes/HeavyMinion/HeavyMinionAI.cs
+++ Assets/DChild/Scripts/Gameplay/Characters/Enemy/MinionTypes/HeavyMinion/HeavyMinionAI.cs
@@ -375,6 +375,7 @@ namespace DChild.Gameplay.Characters.Enemies
             m_chargedDashBB.enabled = true;
             yield return new WaitForAnimationComplete(m_animation.animationState, m_info.attackTackle.animation);
             m_selfCollider.enabled = true;
+            m_bodyCollisionBB.enabled = true;
             m_chargedDashBB.enabled = false;
             m_animation.SetAnimation(0, m_info.idleAnimation, true);
             m_stateHandle.ApplyQueuedState();
