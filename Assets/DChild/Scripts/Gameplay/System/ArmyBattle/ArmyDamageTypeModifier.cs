21641837b33a473dbf423d7382ad8328fc710da2
Juanito Jose Padilla <jjpadilla0915@gmail.com>
2024-09-11 17:53:46 +0800

    [*] Renamed and repurposed IArmyUnitModifier to ArmyDamageTypeModifier and applied GetModifier, SetModifier, and AddModifier functions

---
+++ Assets/DChild/Scripts/Gameplay/System/ArmyBattle/ArmyDamageTypeModifier.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DChild.Gameplay.ArmyBattle
+{
+    public class ArmyDamageTypeModifier
+    {
+        float GetModifier(UnitType unitType)
+        {
+            throw new NotImplementedException();
+        }
+        void SetModifier(UnitType unitType, float value)
+        {
+            throw new NotImplementedException();
+        }
+        void AddModifier(UnitType unitType, float value)
+        {
+            throw new NotImplementedException();
+        }
+        void ResetModifiers()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
