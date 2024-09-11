38a966c1db8ed41fe4e03b47b074aebe90505836
Juanito Jose Padilla <jjpadilla0915@gmail.com>
2024-09-11 12:12:34 +0800

    [*] Created ArmyDamageTypeModifier class

---
+++ Assets/DChild/Scripts/Gameplay/System/ArmyBattle/ArmyBattleSystem/ArmyDamageTypeModifier.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DChild.Gameplay.ArmyBattle
+{
+    public class ArmyDamageTypeModifier
+    {
+        public void GetModifierType(DamageType damageType)
+        {
+
+        }
+
+        public void SetModifierType(DamageType damageType, float value)
+        {
+
+        }
+
+        public void AddModifier(DamageType damageType, float value)
+        {
+
+        }
+    }
+}
+
