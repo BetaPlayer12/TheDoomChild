b9852bc891af5d02aede22f62aa309cbdb233634
Juanito Jose Padilla <jjpadilla0915@gmail.com>
2024-09-12 11:51:22 +0800

    [+] Created Army Modifier script

---
+++ Assets/DChild/Scripts/Gameplay/System/ArmyBattle/ArmyModifier.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace DChild.Gameplay.ArmyBattle
+{
+    public class ArmyModifier 
+    {
+        public ArmyDamageTypeModifier damageModifier;
+        public ArmyDamageTypeModifier resistanceModifier;
+    }
+}
+
