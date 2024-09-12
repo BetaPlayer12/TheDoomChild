b570fec1c227eff84ace879f611143fdaec798aa
Juanito Jose Padilla <jjpadilla0915@gmail.com>
2024-09-12 11:44:14 +0800

    [+] Created new Army Data  script

---
+++ Assets/DChild/Scripts/Gameplay/System/ArmyBattle/ArmyData.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace DChild.Gameplay.ArmyBattle
+{
+    public class ArmyData
+    {
+        public ArmyInfo info;
+    }
+}
+
