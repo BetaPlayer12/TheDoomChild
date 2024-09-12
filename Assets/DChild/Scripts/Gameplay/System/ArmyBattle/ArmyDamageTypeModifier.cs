100f81f41ee67aadb3967422e6dd96f1e1b07bdc
Juanito Jose Padilla <jjpadilla0915@gmail.com>
2024-09-12 19:09:39 +0800

    [*] Made all functions in ArmyDamageTypeModifier public (Did not follow diagram)

--- Assets/DChild/Scripts/Gameplay/System/ArmyBattle/ArmyDamageTypeModifier.cs
+++ Assets/DChild/Scripts/Gameplay/System/ArmyBattle/ArmyDamageTypeModifier.cs
@@ -4,19 +4,19 @@ namespace DChild.Gameplay.ArmyBattle
 {
     public class ArmyDamageTypeModifier
     {
-        float GetModifier(UnitType unitType)
+        public float GetModifier(UnitType unitType)
         {
             throw new NotImplementedException();
         }
-        void SetModifier(UnitType unitType, float value)
+        public void SetModifier(UnitType unitType, float value)
         {
             throw new NotImplementedException();
         }
-        void AddModifier(UnitType unitType, float value)
+        public void AddModifier(UnitType unitType, float value)
         {
             throw new NotImplementedException();
         }
-        void ResetModifiers()
+        public void ResetModifiers()
         {
             throw new NotImplementedException();
         }
