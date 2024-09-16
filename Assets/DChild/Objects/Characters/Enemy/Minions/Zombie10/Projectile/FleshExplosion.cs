10d42585638d7f0a79681d2f8c3b6381ea3d709d
Tommi Pallarco <tommipallarco@gmail.com>
2024-09-16 16:06:49 +0800

    [*] despawn Zombie Gorilla projectiles after end of duration

--- Assets/DChild/Objects/Characters/Enemy/Minions/Zombie10/Projectile/FleshExplosion.cs
+++ Assets/DChild/Objects/Characters/Enemy/Minions/Zombie10/Projectile/FleshExplosion.cs
@@ -6,8 +6,6 @@ using UnityEngine;
 
 public class FleshExplosion : MonoBehaviour
 {
-    /*   [SerializeField]
-       private GameObject[] m_fleshExplosionPieces;*/
     [SerializeField]
     private GameObject m_fleshExplosionFX;
     private void Start()
@@ -16,15 +14,12 @@ public class FleshExplosion : MonoBehaviour
         var force = 40f;
         for(int i = 0; i < fleshPiecesToSpawn; i++)
         {
-           
             var instance = GameSystem.poolManager.GetPool<ProjectilePool>().GetOrCreateItem(m_fleshExplosionFX);
-            //var instance = GameSystem.poolManager.GetPool<PoolableObjectPool>().GetOrCreateItem(m_fleshExplosionFX, gameObject.scene);
             Vector2 spawnPosition = new Vector2(transform.position.x, transform.position.y);
-            instance.SpawnAt(spawnPosition, Quaternion.identity);
-            /*m_fleshExplosionPieces[i].SetActive(true);*/
             var rigidbody = instance.GetComponent<Rigidbody2D>();
-            Vector2 randomDirection = new Vector2(Random.Range(-4f, 4f),Random.Range(2f, 3f)).normalized;
+            Vector2 randomDirection = new Vector2(Random.Range(-4f, 4f) - .1f ,Random.Range(2f, 3f)).normalized;
             rigidbody.AddForce(randomDirection * force, ForceMode2D.Impulse);
+            instance.SpawnAt(spawnPosition, Quaternion.identity);
         }
     }
 
