4ba9e15dd229c9ef23b732447503541d08cb3d8a
Juanito Jose Padilla <jjpadilla0915@gmail.com>
2024-09-17 15:41:46 +0800

    [*] Removed variables and functions in Army script absent from Class Diagram

--- Assets/DChild/Scripts/Gameplay/System/ArmyBattle/Army.cs
+++ Assets/DChild/Scripts/Gameplay/System/ArmyBattle/Army.cs
@@ -8,39 +8,23 @@ namespace DChild.Gameplay.ArmyBattle
 {
     public class Army : MonoBehaviour
     {
-        //#if UNITY_EDITOR
-        [SerializeField, HideInPlayMode]
-        private ArmyCompositionData m_initialComposition;
         //#endif
         [SerializeField]
         private int m_troopCount;
-        [SerializeField, HideInEditorMode, TabGroup("Composition")]
-        private ArmyComposition m_composition;
-        [ShowInInspector, HideInEditorMode, ReadOnly]
-        private ArmyDamageTypeModifier m_powerModifier;
-        [ShowInInspector, HideInEditorMode, ReadOnly]
-        private ArmyDamageTypeModifier m_damageReductionModifier;
 
         [SerializeField]
         private ArmyInfo m_info;
-
-        private List<ArmyAttackGroup> cacheAttackGroup;
-        private List<ArmyAbilityGroup> cacheAbilityGroup;
+        [SerializeField]
+        private List<IAttackingGroup> m_availableAttackingGroups;
         [SerializeField]
         private List<ISpecialSkillGroup> m_availableSpecialSkills;
 
+        public ArmyModifier ArmyModifiers;
         public int troopCount => m_troopCount;
-        public ArmyDamageTypeModifier powerModifier => m_powerModifier;
-        public ArmyDamageTypeModifier damageReductionModifier => m_damageReductionModifier;
 
         public Army(ArmyInfo info)
         {
             m_info = info;
-        }
-
-        public int GetTroopCount()
-        {
-            return m_troopCount;
         }
 
         public int AddTroopCount(int additionalTroops)
@@ -58,12 +42,12 @@ namespace DChild.Gameplay.ArmyBattle
             throw new NotImplementedException(); 
         }
         
-        public List<IAttackGroup> GetAvailableGroups(DamageType damageType)
+        public List<IAttackingGroup> GetAvailableGroups(DamageType damageType)
         {
             throw new NotImplementedException();
         }
 
-        public List<IAttackGroup> SetAttackingGroups(DamageType damageType)
+        public void SetAttackingGroupAvailability(IAttackingGroup attackingGroup, bool isAvailable)
         {
             throw new NotImplementedException();
         }
@@ -78,106 +62,9 @@ namespace DChild.Gameplay.ArmyBattle
             throw new NotImplementedException();
         }
 
-        public List<SpecialSkill> SetSpecialSkillAvailability()
+        public void SetSpecialSkillAvailability(SpecialSkill group, bool isAvailable)
         {
             throw new NotImplementedException();
-        }
-
-        public bool HasAvailableAttackGroup(UnitType unitType)
-        {
-            var groups = m_composition.attacks.GetAttackGroupsOfUnityType(unitType);
-            for (int i = 0; i < groups.Count; i++)
-            {
-                //if (groups[i].isAvailable)
-                //{
-                //    return true;
-                //}
-            }
-
-            return false;
-        }
-
-        public bool HasAvailableAbilityGroup()
-        {
-            var abilityList = m_composition.abilities;
-            for (int i = 0; i < abilityList.count; i++)
-            {
-                var group = abilityList.GetAbilityGroup(i);
-                //if (group.isAvailable)
-                //{
-                //    return true;
-                //}
-            }
-            return false;
-        }
-
-        public List<ArmyAttackGroup> GetAvailableAttackGroups(UnitType unitType)
-        {
-            cacheAttackGroup.Clear();
-            var groups = m_composition.attacks.GetAttackGroupsOfUnityType(unitType);
-            for (int i = 0; i < groups.Count; i++)
-            {
-                var group = groups[i];
-                //if (group.isAvailable)
-                //{
-                //    cacheAttackGroup.Add(group);
-                //}
-            }
-            return cacheAttackGroup;
-        }
-
-        public List<ArmyAbilityGroup> GetAvailableAbilityGroups()
-        {
-            cacheAbilityGroup.Clear();
-            var abilityList = m_composition.abilities;
-            for (int i = 0; i < abilityList.count; i++)
-            {
-                var group = abilityList.GetAbilityGroup(i);
-                //if (group.isAvailable)
-                //{
-                //    cacheAbilityGroup.Add(group);
-                //}
-            }
-
-            return cacheAbilityGroup;
-        }
-
-        public void SetArmyComposition(ArmyComposition armyComposition)
-        {
-            m_composition = armyComposition;
-        }
-
-        public void RecordArmyCompositionTo(ref ArmyComposition armyComposition)
-        {
-            //armyComposition.CopyComposition(m_composition);
-        }
-
-        public void Initialize()
-        {
-            //m_powerModifier = new ArmyUnitModifier(1, 1, 1);
-            //m_damageReductionModifier = new ArmyUnitModifier(0, 0, 0);
-            m_composition.attacks.ResetAvailability();
-        }
-
-        public override string ToString()
-        {
-            return $"{gameObject.name}(Army[{m_composition.name}])";
-        }
-
-        private void Awake()
-        {
-            //#if UNITY_EDITOR
-            if (m_initialComposition != null)
-            {
-                m_composition = m_initialComposition.GenerateArmyCompositionInstance();
-                m_composition.attacks.ResetAvailability();
-            }
-
-            //#endif
-            //m_powerModifier = new ArmyUnitModifier(1, 1, 1);
-            //m_damageReductionModifier = new ArmyUnitModifier(0, 0, 0);
-            cacheAttackGroup = new List<ArmyAttackGroup>();
-            cacheAbilityGroup = new List<ArmyAbilityGroup>();
         }
     }
 }
