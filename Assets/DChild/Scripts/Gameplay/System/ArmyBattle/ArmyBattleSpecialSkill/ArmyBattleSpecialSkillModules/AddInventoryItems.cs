cba55b239bdc6466d71f36b23a5846ae70426520
HolySoft_Ayan <betaplayer12@gmail.com>
2024-09-19 18:08:51 +0800

    [+]Added Other Modules that are needed but are not working yet

--- Assets/DChild/Scripts/Gameplay/System/ArmyBattle/ArmyBattleSpecialSkill/ArmyBattleSpecialSkillModules/AddInventoryItems.cs
+++ Assets/DChild/Scripts/Gameplay/System/ArmyBattle/ArmyBattleSpecialSkill/ArmyBattleSpecialSkillModules/AddInventoryItems.cs
@@ -1,23 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace DChild.Gameplay.ArmyBattle.SpecialSkills.Modules
 {
-    public class DisableUnitType : ISpecialSkillModule, ISpecialSkillImplementor
+    public class AddInventoryItems : ISpecialSkillModule, ISpecialSkillImplementor
     {
-        [SerializeField]
-        private DamageType m_damageType;
         public void ApplyEffect(ArmyController owner, ArmyController target)
         {
-
-            // Should not Be Empty
             Debug.Log("It works now Apply the Effects");
         }
 
         public void RemoveEffect(ArmyController owner, ArmyController target)
         {
-            target.controlledArmy.ResetGroupAvailability();
+
         }
     }
 }
--- Assets/DChild/Scripts/Gameplay/System/ArmyBattle/ArmyBattleSpecialSkill/ArmyBattleSpecialSkillModules/SuccessFailChance.cs
+++ Assets/DChild/Scripts/Gameplay/System/ArmyBattle/ArmyBattleSpecialSkill/ArmyBattleSpecialSkillModules/SuccessFailChance.cs
@@ -1,27 +1,44 @@
+using Sirenix.OdinInspector;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace DChild.Gameplay.ArmyBattle.SpecialSkills.Modules
 {
+    [InfoBox("This will not Remove the Effects of either fail or success results")]
     public class SuccessFailChance : ISpecialSkillModule, ISpecialSkillImplementor
     {
         [SerializeField, Range(0, 100)]
         private float m_successChance;
-        [SerializeField]
-        private bool m_effectSuccessful;
+        [SerializeField, TabGroup("OnSuccess")]
+        private ISpecialSkillModule[] m_onSuccess;
+        [SerializeField, TabGroup("OnFail")]
+        private ISpecialSkillModule[] m_onFail;
+
         public void ApplyEffect(ArmyController owner, ArmyController target)
         {
             float chance = Random.Range(0, 100);
-            if ((chance <= m_successChance))
+            if (chance <= m_successChance)
             {
-                m_effectSuccessful = true;
+                ApplyModules(m_onSuccess, owner, target);
+            }
+            else
+            {
+                ApplyModules(m_onFail, owner, target);
             }
         }
 
         public void RemoveEffect(ArmyController owner, ArmyController target)
         {
+            //Will not have any effect here
+        }
 
+        private void ApplyModules(ISpecialSkillModule[] specialSkillModules, ArmyController owner, ArmyController target)
+        {
+            for (int i = 0; i < specialSkillModules.Length; i++)
+            {
+                specialSkillModules[i].ApplyEffect(owner, target);
+            }
         }
     }
 }
