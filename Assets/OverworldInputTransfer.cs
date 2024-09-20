164e8b11056cb934cf46637b5f794cb5cccf9dc0
JJ Besa <hybrev300@gmail.com>
2024-09-20 16:00:02 +0800

    [*] adjusted overworld input switch to underworld logic [*] applied script as component to overworld zee instead of portal

--- Assets/OverworldInputTransfer.cs
+++ Assets/OverworldInputTransfer.cs
@@ -8,22 +8,10 @@ public class OverworldInputTransfer: MonoBehaviour
 {
     private PlayerInput m_overworldInput;
 
-    private void OnDisable()
-    {
-        m_overworldInput = GetComponentInParent<PlayerInput>(true);
-        Debug.Log($"overworld input: {m_overworldInput}");
-        m_overworldInput.enabled = false;
-
-        StartCoroutine(SwitchToUnderworldControlsRoutine());
-    }
-
-    private static IEnumerator SwitchToUnderworldControlsRoutine()
+    private void OnDestroy()
     {
         PlayerInput m_playerInput = GameplaySystem.playerManager.player.GetComponentInChildren<PlayerInput>(true);
-        yield return new WaitForSeconds(0.5f);
         m_playerInput.enabled = true;
-        Debug.Log("successfully switched over to under controls");
-
-
+        Debug.Log("successfully switched over to underworld controls");
     }
 }
