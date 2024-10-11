32dd41d65121f7da30dff39b6de14c689ec2cb1c
JJ Besa <hybrev300@gmail.com>
2024-10-11 18:59:25 +0800

    [*]army banner script config [*] party name label reposition

--- Assets/DChild/Scripts/Gameplay/System/ArmyBattle/ArmyBattleUI/ArmyBannerUI.cs
+++ Assets/DChild/Scripts/Gameplay/System/ArmyBattle/ArmyBattleUI/ArmyBannerUI.cs
@@ -1,11 +1,20 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace DChild.Gameplay.ArmyBattle.UI
 {
     public class ArmyBannerUI : MonoBehaviour
     {
+        [SerializeField]
+        private TextMeshProUGUI m_armyName;
+        [SerializeField]
+        private Image m_armyIcon;
+
         public void Display(ArmyOverviewData overviewData)
         {
+            m_armyName.text = overviewData.name.ToUpper();
+            m_armyIcon.sprite = overviewData.icon;
             Debug.Log($"ArmyBattle: Displaying {overviewData.name} in Banner");
         }
     }
