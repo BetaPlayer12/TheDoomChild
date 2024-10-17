9fece1b392a09d4880e13fc9df1f6750181eff5c
JJ Besa <hybrev300@gmail.com>
2024-10-17 14:43:19 +0800

    [+] selected skill button script [*] execute attack window animation polishing [*] army battle flow refactoring [*] army character icon displaying using asset data

--- Assets/DChild/Scripts/Gameplay/System/ArmyBattle/ArmyBattleUI/AttackingGroupOptionUI.cs
+++ Assets/DChild/Scripts/Gameplay/System/ArmyBattle/ArmyBattleUI/AttackingGroupOptionUI.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 namespace DChild.Gameplay.ArmyBattle.UI
 {
@@ -10,13 +11,15 @@ namespace DChild.Gameplay.ArmyBattle.UI
         private ArmyCharacterGroupUI m_characterGroupUI;
         [SerializeField]
         private AttackingGroupPowerUI m_attackPowerUI;
-     
+        [SerializeField]
+        private SelectedSkillButton m_selectedSkill;
 
         public void Display(IAttackingGroup group)
         {
             m_partyName.Display(group);
             m_characterGroupUI.Display(group?.GetCharacterGroup() ?? null);
             m_attackPowerUI.Display(group);
+            m_selectedSkill.Display(group.GetDamageType());
         }
     }
 }
