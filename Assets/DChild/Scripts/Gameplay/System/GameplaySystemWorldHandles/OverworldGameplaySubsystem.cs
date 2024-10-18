f02882109cb509639464949451aa33f4ce97a1f9
Juanito Jose Padilla <jjpadilla0915@gmail.com>
2024-10-18 11:58:15 +0800

    [+] Added Gameplay Subsystem scripts

---
+++ Assets/DChild/Scripts/Gameplay/System/GameplaySystemWorldHandles/OverworldGameplaySubsystem.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DChild.Gameplay.Systems
+{
+    public class OverworldGameplaySubsystem : MonoBehaviour
+    {
+
+        #region Modules
+        private static GameplayUIHandle m_gameplayUIHandle;
+        #endregion
+    }
+}
+
