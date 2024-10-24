537e6489374d2366a4388285410cf299b3a6d638
JJ Besa <hybrev300@gmail.com>
2024-10-24 14:46:50 +0800

    [+] army battle start-end popup banner script [*] reworded battle end signal in UI database for consistency [*] attached aforementioned script to popup prefab [*] adjusted UI flow elements to require battleEnd over battleFinish

---
+++ Assets/DChild/Scripts/Gameplay/System/ArmyBattle/ArmyBattleUI/ArmyBattlePopupUI.cs
@@ -0,0 +1,59 @@
+using Doozy.Runtime.Signals;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace DChild.Gameplay.ArmyBattle.UI
+{
+    public class ArmyBattlePopupUI : MonoBehaviour
+    {
+        [SerializeField]
+        private string m_signalCategory;
+        [SerializeField]
+        private string m_signalName;
+        [SerializeField]
+        private TextMeshProUGUI m_popupLabel;
+
+
+        private SignalReceiver m_signalReceiver;
+        private SignalStream m_signalStream;
+
+        private void Awake()
+        {
+            m_signalStream = SignalStream.Get(m_signalCategory, m_signalName);
+            m_signalReceiver = new SignalReceiver().SetOnSignalCallback(OnSignal);
+        }
+
+        private void OnEnable()
+        {
+            m_signalStream.ConnectReceiver(m_signalReceiver);
+        }
+
+        private void OnDisable()
+        {
+            m_signalStream.DisconnectReceiver(m_signalReceiver);
+        }
+
+        private void OnSignal(Signal signal)
+        {
+            Debug.Log($"received data type was: {signal.valueType}");
+
+            if (signal.valueType != typeof(bool))
+            {
+                m_popupLabel.text = "BATTLE";
+                return;
+            }
+
+            bool battleResult = (bool)signal.valueAsObject;
+            Debug.Log($"received data value was: {signal.valueAsObject}");
+
+            if (battleResult != true)
+            {
+                m_popupLabel.text = "DEFEAT";
+                return;
+            }
+            m_popupLabel.text = "VICTORY";
+        }
+    }
+}
